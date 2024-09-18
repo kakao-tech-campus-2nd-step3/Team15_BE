f40dc52dc4c384cbe27056f3fed65f6096f4a374
Profile-exe <kang67572346@gmail.com>
2024-09-18 19:26:14 +0900

    init: 초기 프로젝트 세팅

---
+++ src/main/java/kakao/rebit/RebitApplication.java
@@ -0,0 +1,13 @@
+package kakao.rebit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebitApplication.class, args);
+    }
+
+}
