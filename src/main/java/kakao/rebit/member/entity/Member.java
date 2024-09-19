82fdb8e5c8fc500aeead286d3d12d2221877f1e6
Profile-exe <kang67572346@gmail.com>
2024-09-19 21:26:23 +0900

    feat: Member 엔티티 구현

---
+++ src/main/java/kakao/rebit/member/entity/Member.java
@@ -0,0 +1,58 @@
+package kakao.rebit.member.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import kakao.rebit.common.persistence.BaseEntity;
+
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String imageUrl;
+
+    private String bio;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private Integer point;
+
+    private String kakaoToken;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public String getKakaoToken() {
+        return kakaoToken;
+    }
+}
