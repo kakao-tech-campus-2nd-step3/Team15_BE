2600158fb81c4fb38e560603e4f8a65503c7eb32
Profile-exe <kang67572346@gmail.com>
2024-09-19 21:01:07 +0900

    feat: BaseEntity 구현
    - JPA Auditing을 적용하기 위한 BaseEntity 구현
    - createdAt, modifiedAt 필드로 엔티티 추적 용이

--- src/main/java/kakao/rebit/RebitApplication.java
+++ src/main/java/kakao/rebit/RebitApplication.java
@@ -2,7 +2,9 @@ package kakao.rebit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class RebitApplication {
 
---
+++ src/main/java/kakao/rebit/common/persistence/BaseEntity.java
@@ -0,0 +1,30 @@
+package kakao.rebit.common.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+}
