e137c9efe0ed795261eacf5c90b465c393b97a09
안원모 <tommy45231@naver.com>
2024-09-20 22:51:21 +0900

    refactor: 부모인 Wishlist를 상속받도록 코드 리팩토링

--- src/main/java/kakao/rebit/wishlist/entity/BookWishlist.java
+++ src/main/java/kakao/rebit/wishlist/entity/BookWishlist.java
@@ -2,21 +2,14 @@ package kakao.rebit.wishlist.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
-import kakao.rebit.common.persistence.BaseEntity;
 import kakao.rebit.member.entity.Member;
 
 @Entity
 @DiscriminatorValue("BOOK")
 @Table(name = "book_wishes")
 public class BookWishlist extends Wishlist {
-    
+
     private String isbn;
 
     protected BookWishlist() {
