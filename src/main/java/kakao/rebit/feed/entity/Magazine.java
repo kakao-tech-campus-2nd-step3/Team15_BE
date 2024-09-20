7594346928321cc6d18ada27b74ea4219019c5ff
Kangboom <jkl6147@naver.com>
2024-09-20 17:51:24 +0900

    feat: Magazine 엔티티 구현

---
+++ src/main/java/kakao/rebit/feed/entity/Magazine.java
@@ -0,0 +1,40 @@
+package kakao.rebit.feed.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import kakao.rebit.member.entity.Member;
+
+@Entity
+@Table(name = "magazine")
+@DiscriminatorValue("M")
+public class Magazine extends Feed {
+
+    private String name;
+    private String imageUrl;
+    private String content;
+
+    protected Magazine() {
+
+    }
+
+    public Magazine(Member member, int likes, Book book, String name, String imageUrl,
+            String content) {
+        super(member, likes, book);
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
