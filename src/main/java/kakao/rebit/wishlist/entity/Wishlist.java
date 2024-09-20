c12f856a97aae43c8630b29679b645d9cda46ef4
안원모 <tommy45231@naver.com>
2024-09-20 23:48:47 +0900

    refactor : erd 명세에 맞도록 컬럼명 수정

--- src/main/java/kakao/rebit/wishlist/entity/Wishlist.java
+++ src/main/java/kakao/rebit/wishlist/entity/Wishlist.java
@@ -25,7 +25,7 @@ public class Wishlist extends BaseEntity {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     protected Wishlist() {
