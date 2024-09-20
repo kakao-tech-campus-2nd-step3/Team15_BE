8f833bcf72eeb16b760220caf325e61764ec263d
seolbin96 <jsb3038@jnu.ac.kr>
2024-09-20 17:14:07 +0900

    독서일기 엔티티 구현

---
+++ src/main/java/kakao/rebit/diary/entity/Diary.java
@@ -0,0 +1,47 @@
+package kakao.rebit.diary.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import kakao.rebit.common.persistence.BaseEntity;
+import kakao.rebit.member.entity.Member;
+
+@Entity
+@Table(name = "diary")
+public class Diary extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 1000)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    protected Diary() {
+    }
+
+    public Diary(String content, Member member) {
+        this.content = content;
+        this.member = member;
+    }
+
+    public Long getId() { return id; }
+
+    public String getContent() { return content; }
+
+    public void setContent(String content) { this.content = content; }
+
+    public Member getMember() { return member; }
+
+    public void setMember(Member member) { this.member = member; }
+}
