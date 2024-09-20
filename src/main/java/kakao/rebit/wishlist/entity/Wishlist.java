d76703672919d6aa3de1f06e7e87b6ef8d4deb90
안원모 <tommy45231@naver.com>
2024-09-20 22:34:50 +0900

    refactor : 위시리스트 구조에서 상속을 적용 Wishlist 부모 클래스로 정의

--- src/main/java/kakao/rebit/wishlist/entity/Wishlist.java
+++ src/main/java/kakao/rebit/wishlist/entity/Wishlist.java
@@ -1,9 +1,12 @@
 package kakao.rebit.wishlist.entity;
 
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.FetchType;
@@ -12,6 +15,8 @@ import kakao.rebit.common.persistence.BaseEntity;
 import kakao.rebit.member.entity.Member;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "type")
 @Table(name = "wishlist")
 public class Wishlist extends BaseEntity {
 
