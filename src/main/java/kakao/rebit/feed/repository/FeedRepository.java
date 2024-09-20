9cbefa40ead405a98cefc7a9980e4cd708c6e6e5
Kangboom <jkl6147@naver.com>
2024-09-20 17:56:28 +0900

    chore: 불필요한 import 삭제

--- src/main/java/kakao/rebit/feed/repository/FeedRepository.java
+++ src/main/java/kakao/rebit/feed/repository/FeedRepository.java
@@ -1,6 +1,5 @@
 package kakao.rebit.feed.repository;
 
-import java.util.List;
 import kakao.rebit.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
