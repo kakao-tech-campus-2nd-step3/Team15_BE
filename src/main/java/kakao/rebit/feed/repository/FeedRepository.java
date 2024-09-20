fc3683377c5e464dfab730feea7301a16d4b449f
Kangboom <jkl6147@naver.com>
2024-09-20 16:55:40 +0900

    feat: FeedRepository 구현

---
+++ src/main/java/kakao/rebit/feed/repository/FeedRepository.java
@@ -0,0 +1,10 @@
+package kakao.rebit.feed.repository;
+
+import java.util.List;
+import kakao.rebit.feed.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+}
