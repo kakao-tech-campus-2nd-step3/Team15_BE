489b937c2b19491ed1d8d1513dd582ec9190692a
안원모 <tommy45231@naver.com>
2024-09-20 16:37:33 +0900

    feat : ChallengeCertification 엔티티 구현

---
+++ src/main/java/kakao/rebit/challengecertification/entity/ChallengeCertification.java
@@ -0,0 +1,58 @@
+package kakao.rebit.challengecertification.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import kakao.rebit.challenge.entity.Challenge;
+import kakao.rebit.common.persistence.BaseEntity;
+
+@Entity
+@Table(name = "challenge_certification")
+public class ChallengeCertification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String imageUrl;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+    
+    public ChallengeCertification(String title, String imageUrl, String content, Challenge challenge) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.content = content;
+        this.challenge = challenge;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Challenge getChallenge() {
+        return challenge;
+    }
+}
