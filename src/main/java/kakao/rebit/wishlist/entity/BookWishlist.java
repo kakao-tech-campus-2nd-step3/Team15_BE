cd9dd4e89192b8eb3c0a08b54cc058b381c64be9
안원모 <tommy45231@naver.com>
2024-09-20 17:09:06 +0900

    feat: ChallengeWishlistRepository 저장소 구현

---
+++ src/main/java/kakao/rebit/wishlist/entity/BookWishlist.java
@@ -0,0 +1,46 @@
+package kakao.rebit.wishlist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import kakao.rebit.common.persistence.BaseEntity;
+
+@Entity
+@Table(name = "book_wishes")
+public class BookWishlist extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wishlist_id")
+    private Wishlist wishlist;
+
+    private String isbn;
+
+    protected BookWishlist() {
+    }
+
+    public BookWishlist(Wishlist wishlist, String isbn) {
+        this.wishlist = wishlist;
+        this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Wishlist getWishlist() {
+        return wishlist;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+}
---
+++ src/main/java/kakao/rebit/wishlist/repository/ChallengeWishlistRepository.java
@@ -0,0 +1,8 @@
+package kakao.rebit.wishlist.repository;
+
+import kakao.rebit.wishlist.entity.ChallengeWishlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeWishlistRepository extends JpaRepository<ChallengeWishlist, Long> {
+
+}
