4b4d7181c3c713a5617e043fcb2bd9da078eb185
Wonmoan <102709424+Wonmoan@users.noreply.github.com>
2024-09-20 23:52:33 +0900

    [FEAT] 위시리스트, 챌린지 인증글 엔티티 구현 (#7)
    * feat : ChallengeCertification 엔티티 구현
    
    * feat : ChallengeCertificationRepository 구현
    
    * fix: ChallengerCertification 엔티티 기본생성자 추가
    
    * feat: Wishlist 도메인 엔티티 구현
    
    * feat : WishlistRepository 구현
    
    * feat: ChallengeWishlist 도메인 구현
    
    * feat: ChallengeWishlistRepository 저장소 구현
    
    * feat: BookWishlistRepository 저장소 구현
    
    * feat: BookWishlist 앤티티 구현
    
    * refactor : 위시리스트 구조에서 상속을 적용 Wishlist 부모 클래스로 정의
    
    * refactor: 부모인 Wishlist를 상속받도록 BookWishlist 코드 리팩토링
    
    * refactor: 부모인 Wishlist를 상속받도록 코드 리팩토링
    
    * refactor: BookWishlist와 book 엔티티와 연관관계 매핑
    
    * refactor : erd 명세에 맞도록 컬럼명 수정

---
+++ src/main/java/kakao/rebit/challengecertification/repository/ChallengeCertificationRepository.java
@@ -0,0 +1,8 @@
+package kakao.rebit.challengecertification.repository;
+
+import kakao.rebit.challengecertification.entity.ChallengeCertification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeCertificationRepository extends JpaRepository<ChallengeCertification, Long> {
+
+}
---
+++ src/main/java/kakao/rebit/wishlist/entity/ChallengeWishlist.java
@@ -0,0 +1,32 @@
+package kakao.rebit.wishlist.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Table;
+import kakao.rebit.challenge.entity.Challenge;
+import kakao.rebit.member.entity.Member;
+
+@Entity
+@DiscriminatorValue("CHALLENGE")
+@Table(name = "challenge_wishes")
+public class ChallengeWishlist extends Wishlist {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id")
+    private Challenge challenge;
+
+    protected ChallengeWishlist() {
+    }
+
+    public ChallengeWishlist(Member member, Challenge challenge) {
+        super(member);
+        this.challenge = challenge;
+    }
+
+    public Challenge getChallenge() {
+        return challenge;
+    }
+}
