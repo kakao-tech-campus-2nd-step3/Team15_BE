a43375d413adc53d1d831072572613e2cabe4a65
Kangboom <jkl6147@naver.com>
2024-09-20 16:54:41 +0900

    feat: FavoriteBook 엔티티 구현

---
+++ src/main/java/kakao/rebit/feed/entity/FavoriteBook.java
@@ -0,0 +1,30 @@
+package kakao.rebit.feed.entity;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import kakao.rebit.member.entity.Member;
+
+@Entity
+@DiscriminatorValue("FB")
+public class FavoriteBook extends Feed{
+    private String brief_review;
+
+    private String full_review;
+
+    protected FavoriteBook(){
+    }
+
+    public FavoriteBook(Long id, Member member, int likes, Book book, String brief_review, String full_review) {
+        super(id, member, likes, book);
+        this.brief_review = brief_review;
+        this.full_review = full_review;
+    }
+
+    public String getBrief_review() {
+        return brief_review;
+    }
+
+    public String getFull_review() {
+        return full_review;
+    }
+}
