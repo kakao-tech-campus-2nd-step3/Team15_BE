67b3c031fd6d8d175ede3c6d50934b416f9e34f7
안원모 <tommy45231@naver.com>
2024-09-20 22:45:59 +0900

    refactor: 부모인 Wishlist를 상속받도록 BookWishlist 코드 리팩토링

--- src/main/java/kakao/rebit/wishlist/entity/BookWishlist.java
+++ src/main/java/kakao/rebit/wishlist/entity/BookWishlist.java
@@ -1,5 +1,6 @@
 package kakao.rebit.wishlist.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,35 +10,21 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
 import kakao.rebit.common.persistence.BaseEntity;
+import kakao.rebit.member.entity.Member;
 
 @Entity
+@DiscriminatorValue("BOOK")
 @Table(name = "book_wishes")
-public class BookWishlist extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "wishlist_id")
-    private Wishlist wishlist;
-
+public class BookWishlist extends Wishlist {
+    
     private String isbn;
 
     protected BookWishlist() {
     }
 
-    public BookWishlist(Wishlist wishlist, String isbn) {
-        this.wishlist = wishlist;
+    public BookWishlist(Member member, String isbn) {
+        super(member);
         this.isbn = isbn;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Wishlist getWishlist() {
-        return wishlist;
     }
 
     public String getIsbn() {
--- src/main/java/kakao/rebit/wishlist/entity/ChallengeWishlist.java
+++ src/main/java/kakao/rebit/wishlist/entity/ChallengeWishlist.java
@@ -1,27 +1,18 @@
 package kakao.rebit.wishlist.entity;
 
+import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Table;
-import kakao.rebit.common.persistence.BaseEntity;
 import kakao.rebit.challenge.entity.Challenge;
+import kakao.rebit.member.entity.Member;
 
 @Entity
+@DiscriminatorValue("CHALLENGE")
 @Table(name = "challenge_wishes")
-public class ChallengeWishlist extends BaseEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "wishlist_id")
-    private Wishlist wishlist;
+public class ChallengeWishlist extends Wishlist {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "challenge_id")
@@ -30,17 +21,9 @@ public class ChallengeWishlist extends BaseEntity {
     protected ChallengeWishlist() {
     }
 
-    public ChallengeWishlist(Wishlist wishlist, Challenge challenge) {
-        this.wishlist = wishlist;
+    public ChallengeWishlist(Member member, Challenge challenge) {
+        super(member);
         this.challenge = challenge;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public Wishlist getWishlist() {
-        return wishlist;
     }
 
     public Challenge getChallenge() {
