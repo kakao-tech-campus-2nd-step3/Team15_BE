e67f1ff38c12c30abc31f42f3ade46ecb903bbe8
seolbin96 <jsb3038@jnu.ac.kr>
2024-09-20 17:21:54 +0900

    fix : book getter-setter 추가

--- src/main/java/kakao/rebit/diary/entity/Diary.java
+++ src/main/java/kakao/rebit/diary/entity/Diary.java
@@ -50,4 +50,8 @@ public class Diary extends BaseEntity {
     public Member getMember() { return member; }
 
     public void setMember(Member member) { this.member = member; }
+
+    public Book getBook() { return book; }
+
+    public void setBook(Book book) { this.book = book; }
 }
