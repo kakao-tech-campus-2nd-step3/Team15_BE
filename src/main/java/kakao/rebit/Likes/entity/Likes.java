084238d79196fdae3edd90fc9709c1d1f2e64c11
Kangboom <jkl6147@naver.com>
2024-09-20 21:56:09 +0900

    fix: 연관관계 수정

--- src/main/java/kakao/rebit/Likes/entity/Likes.java
+++ src/main/java/kakao/rebit/Likes/entity/Likes.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import kakao.rebit.feed.entity.Feed;
 import kakao.rebit.member.entity.Member;
@@ -19,11 +19,11 @@ public class Likes {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
     private Feed feed;
 
