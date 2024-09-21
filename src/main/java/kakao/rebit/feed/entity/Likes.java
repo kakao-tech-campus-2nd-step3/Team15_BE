0744b359cd5a29e2eb9a90e2a65acda5a3f7d701
Profile-exe <kang67572346@gmail.com>
2024-09-21 11:54:20 +0900

    refactor: 좋아요 패키지 변경

--- src/main/java/kakao/rebit/feed/entity/Likes.java
+++ src/main/java/kakao/rebit/feed/entity/Likes.java
@@ -1,4 +1,4 @@
-package kakao.rebit.Likes.entity;
+package kakao.rebit.feed.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -8,7 +8,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import kakao.rebit.feed.entity.Feed;
 import kakao.rebit.member.entity.Member;
 
 @Entity
