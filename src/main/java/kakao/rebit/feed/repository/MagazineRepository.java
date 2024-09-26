ea66b96b7e7da8de1a7de4d5c775fe7b83bc13a0
Kangboom <jkl6147@naver.com>
2024-09-27 01:07:49 +0900

    refactor: 불필요한 어노테이션 생략

--- src/main/java/kakao/rebit/feed/repository/MagazineRepository.java
+++ src/main/java/kakao/rebit/feed/repository/MagazineRepository.java
@@ -2,9 +2,7 @@ package kakao.rebit.feed.repository;
 
 import kakao.rebit.feed.entity.Magazine;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MagazineRepository extends JpaRepository<Magazine, Long> {
 
 }
