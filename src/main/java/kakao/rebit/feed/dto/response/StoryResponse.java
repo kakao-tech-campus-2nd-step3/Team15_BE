ab193cb1178eda61036f4603fbc3eb4783dcccec
Kangboom <jkl6147@naver.com>
2024-09-26 17:40:53 +0900

    feat: 스토리 응답을 위한 DTO 구현

---
+++ src/main/java/kakao/rebit/feed/dto/response/StoryResponse.java
@@ -0,0 +1,24 @@
+package kakao.rebit.feed.dto.response;
+
+import kakao.rebit.member.dto.MemberResponse;
+
+public class StoryResponse extends FeedResponse {
+
+    private String imageUrl;
+    private String content;
+
+    public StoryResponse(Long id, MemberResponse memberResponse, BookResponse bookResponse,
+            String imageUrl, String content) {
+        super(id, memberResponse, bookResponse, Type.S);
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
