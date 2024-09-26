c9d014ddce5172ecba70d1c5f4dba8d7c9ee74a8
Kangboom <jkl6147@naver.com>
2024-09-26 17:41:02 +0900

    feat: 메거진 응답을 위한 DTO 구현

---
+++ src/main/java/kakao/rebit/feed/dto/response/MagazineResponse.java
@@ -0,0 +1,31 @@
+package kakao.rebit.feed.dto.response;
+
+import kakao.rebit.member.dto.MemberResponse;
+
+public class MagazineResponse extends FeedResponse {
+
+    private String name;
+    private String imageUrl;
+    private String content;
+
+    public MagazineResponse(Long id, MemberResponse memberResponse, BookResponse bookResponse,
+            String name, String imageUrl,
+            String content) {
+        super(id, memberResponse, bookResponse, Type.M);
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
