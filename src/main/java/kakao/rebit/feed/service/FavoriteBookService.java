be00db9e78edc3628084477dfbcd7fd31c090921
Kangboom <jkl6147@naver.com>
2024-09-26 17:38:28 +0900

    feat: 인생책 서비스 구현

---
+++ src/main/java/kakao/rebit/feed/service/FavoriteBookService.java
@@ -0,0 +1,60 @@
+package kakao.rebit.feed.service;
+
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.feed.dto.response.BookResponse;
+import kakao.rebit.feed.dto.response.FavoriteBookResponse;
+import kakao.rebit.feed.entity.FavoriteBook;
+import kakao.rebit.feed.repository.FavoriteBookRepository;
+import kakao.rebit.member.dto.MemberResponse;
+import kakao.rebit.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class FavoriteBookService {
+
+    private final FavoriteBookRepository favoriteBookRepository;
+
+    public FavoriteBookService(FavoriteBookRepository favoriteBookRepository) {
+        this.favoriteBookRepository = favoriteBookRepository;
+    }
+
+    public Page<FavoriteBookResponse> getFavorites(Pageable pageable) {
+        Page<FavoriteBook> favorites = favoriteBookRepository.findAll(pageable);
+        System.out.println(favorites);
+        return favorites.map(this::toFavoriteBookResponse);
+    }
+
+    public FavoriteBookResponse getFavoriteById(Long id) {
+        FavoriteBook favoriteBook = favoriteBookRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("찾는 인생책이 없습니다."));
+        return toFavoriteBookResponse(favoriteBook);
+    }
+
+    /**
+     * 아래부터는 DTO 변환 로직
+     */
+
+    private FavoriteBookResponse toFavoriteBookResponse(FavoriteBook favoriteBook) {
+        return new FavoriteBookResponse(
+                favoriteBook.getId(),
+                toMemberResponse(favoriteBook.getMember()),
+                toBookResponse(favoriteBook.getBook()),
+                favoriteBook.getBriefReview(),
+                favoriteBook.getFullReview()
+        );
+    }
+
+    private MemberResponse toMemberResponse(Member member) {
+        return new MemberResponse(member.getId(), member.getNickname(), member.getImageUrl(),
+                member.getBio(), member.getRole(), member.getPoint());
+    }
+
+    private BookResponse toBookResponse(Book book) {
+        return new BookResponse(book.getId(), book.getIsbn(), book.getTitle(),
+                book.getDescription(), book.getAuthor(), book.getPublisher(), book.getImageUrl());
+    }
+}
