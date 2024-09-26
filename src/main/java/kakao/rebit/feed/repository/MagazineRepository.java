6a7031481cbd6f049f0492e9fb86a7770151080a
Kangboom <jkl6147@naver.com>
2024-09-26 17:40:35 +0900

    refactor: 메거진 레파지터리 JpaRepository 상속

--- src/main/java/kakao/rebit/feed/repository/MagazineRepository.java
+++ src/main/java/kakao/rebit/feed/repository/MagazineRepository.java
@@ -1,8 +1,10 @@
 package kakao.rebit.feed.repository;
 
+import kakao.rebit.feed.entity.Magazine;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MagazineRepository {
+public interface MagazineRepository extends JpaRepository<Magazine, Long> {
 
 }
