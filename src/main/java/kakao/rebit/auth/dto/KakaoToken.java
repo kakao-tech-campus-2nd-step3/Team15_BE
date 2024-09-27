feed066b46b1e8134d52abe3e44e5a986b12a9c8
seolbin96 <jsb3038@jnu.ac.kr>
2024-09-27 22:47:38 +0900

    feat : KakaoToken dto 추가

---
+++ src/main/java/kakao/rebit/auth/dto/KakaoToken.java
@@ -0,0 +1,41 @@
+package kakao.rebit.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class KakaoToken {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("scope")
+    private String scope;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+}
