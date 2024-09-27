913fb4e1c43ea337af0671ac7e2ef5691aa6a72d
안원모 <tommy45231@naver.com>
2024-09-27 17:25:18 +0900

    feat: 책 서비스 구현

---
+++ src/main/java/kakao/rebit/book/service/BookService.java
@@ -0,0 +1,24 @@
+package kakao.rebit.book.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.book.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+    private final AladinApiService aladinApiService;
+
+    @Autowired
+    public BookService(BookRepository bookRepository, AladinApiService aladinApiService) {
+        this.bookRepository = bookRepository;
+        this.aladinApiService = aladinApiService;
+    }
+}
