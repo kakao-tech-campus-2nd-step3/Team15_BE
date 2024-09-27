b5fc6801fe475b3a89d4c42163543cd1bfa7fa5e
seolbin96 <jsb3038@jnu.ac.kr>
2024-09-27 19:21:06 +0900

    feat : 카카오 로그인 서비스 클래스 구현

---
+++ src/main/java/kakao/rebit/auth/service/KakaoAuthService.java
@@ -0,0 +1,55 @@
+package kakao.rebit.auth.service;
+
+import kakao.rebit.auth.Token.AuthTokenGenerator;
+import kakao.rebit.auth.dto.LoginResponse;
+import kakao.rebit.auth.Token.AuthToken;
+import kakao.rebit.member.entity.Member;
+import kakao.rebit.member.entity.Role;
+import kakao.rebit.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Optional;
+
+@Service
+public class KakaoAuthService {
+
+    private final KakaoApiClient kakaoApiClient;
+    private final MemberRepository memberRepository;
+    private final AuthTokenGenerator authTokensGenerator;
+
+    @Autowired
+    public KakaoAuthService(KakaoApiClient kakaoApiClient, MemberRepository memberRepository,
+        AuthTokenGenerator authTokensGenerator) {
+        this.kakaoApiClient = kakaoApiClient;
+        this.memberRepository = memberRepository;
+        this.authTokensGenerator = authTokensGenerator;
+    }
+
+    public LoginResponse kakaoLogin(String code, String currentDomain) {
+        // 1. 카카오로부터 액세스 토큰을 받음
+        String accessToken = kakaoApiClient.getAccessToken(code);
+
+        // 2. 받은 액세스 토큰으로 유저 정보를 가져옴
+        HashMap<String, Object> userInfo = kakaoApiClient.getUserInfo(accessToken);
+        Long kakaoId = (Long) userInfo.get("id");
+        String nickname = (String) userInfo.get("nickname");
+
+        // 3. 카카오 토큰으로 기존 유저 조회
+        Optional<Member> memberOptional = memberRepository.findByKakaoToken(accessToken);
+
+        Member member = memberOptional.orElseGet(() -> {
+            // 회원이 없으면 회원가입 처리, 기본 Role은 ROLE_USER로 설정
+            Member newMember = new Member(nickname, null, null, Role.ROLE_USER, null, accessToken);
+            memberRepository.save(newMember);
+            return newMember;
+        });
+
+        // 4. 자체 JWT 토큰 생성
+        AuthToken tokens = authTokensGenerator.generate(member.getId().toString());
+
+        // 5. 로그인 응답 반환
+        return new LoginResponse(tokens);
+    }
+}
