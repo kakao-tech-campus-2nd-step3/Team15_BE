70894f0d0ce8fb09ffb8cfc1da3975e6c04840d9
안원모 <tommy45231@naver.com>
2024-09-27 17:13:13 +0900

    feat: 책 컨트롤러 구현

---
+++ src/main/java/kakao/rebit/book/controller/BookController.java
@@ -0,0 +1,38 @@
+package kakao.rebit.book.controller;
+
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.book.entity.Review;
+import kakao.rebit.book.repository.BookRepository;
+import kakao.rebit.book.service.AladinApiService;
+import kakao.rebit.book.service.BookService;
+import kakao.rebit.book.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class BookController {
+
+    private final BookService bookService;
+    private final ReviewService reviewService;
+    private final AladinApiService aladinApiService;
+    private final BookRepository bookRepository; // 추가
+
+    @Autowired
+    public BookController(BookService bookService, ReviewService reviewService, AladinApiService aladinApiService,BookRepository bookRepository) {
+        this.bookService = bookService;
+        this.reviewService = reviewService;
+        this.aladinApiService = aladinApiService;
+        this.bookRepository = bookRepository; // 추가
+    }
+
+    // 저장된 책 목록 조회
+    @GetMapping("/books/all")
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
