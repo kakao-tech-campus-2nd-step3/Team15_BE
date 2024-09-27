53cce5061d72dccef4b15262e713107e936ff3f4
안원모 <tommy45231@naver.com>
2024-09-27 17:32:25 +0900

    feat: 알라딘 api 파싱 기능 구현

--- src/main/java/kakao/rebit/book/service/BookService.java
+++ src/main/java/kakao/rebit/book/service/BookService.java
@@ -40,9 +40,54 @@ public class BookService {
             });
         return savedBook;
     }
-    
+
     public Book getBookDetail(String isbn) {
         return bookRepository.findByIsbn(isbn)
             .orElseGet(() -> searchBookByIsbn(isbn));
     }
+
+    private List<Book> parseBooks(String apiResponse) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<String, Object> responseMap = objectMapper.readValue(apiResponse,
+                new TypeReference<Map<String, Object>>() {
+                });
+            return objectMapper.convertValue(responseMap.get("data"),
+                new TypeReference<List<Book>>() {
+                });
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답에서 책 데이터를 파싱하는 데 실패했습니다", e);
+        }
+    }
+
+    private Book parseBookDetail(String apiResponse) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            Map<String, Object> responseMap = objectMapper.readValue(apiResponse,
+                new TypeReference<Map<String, Object>>() {
+                });
+
+            // 알라딘  api 에서 책 관련 정보는 'item' 필드 배열로 반환되어 전송 됨
+            List<Map<String, Object>> items = (List<Map<String, Object>>) responseMap.get("item");
+
+            if (items == null || items.isEmpty()) {
+                throw new RuntimeException("API 응답에 책 정보가 없습니다.");
+            }
+
+            Map<String, Object> item = items.get(0);
+
+            String isbn = (String) item.get("isbn");
+            String title = (String) item.get("title");
+            String description = (String) item.get("description");
+            String author = (String) item.get("author");
+            String publisher = (String) item.get("publisher");
+            String imageUrl = (String) item.get("cover");
+
+            return new Book(isbn, title, description, author, publisher, imageUrl);
+
+        } catch (IOException e) {
+            throw new RuntimeException("API 응답에서 책 상세 정보를 파싱하는 데 실패했습니다", e);
+        }
+    }
+
 }
