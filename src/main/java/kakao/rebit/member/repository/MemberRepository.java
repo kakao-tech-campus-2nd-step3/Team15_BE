0d099d1db5fe8e97079bb768eb97848ef737a715
seolbin96 <jsb3038@jnu.ac.kr>
2024-09-27 21:54:16 +0900

    fix : 토큰 대신 이메일로 사용자를 조회하도록 메소드 수정

--- src/main/java/kakao/rebit/member/repository/MemberRepository.java
+++ src/main/java/kakao/rebit/member/repository/MemberRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByKakaoToken(String kakaoToken);
+    Optional<Member> findByEmail(String email);
 
 }
