963352ed7df66a81e23caddae2803fefbb6e8315
안원모 <tommy45231@naver.com>
2024-09-27 19:56:25 +0900

    refactor: 중복 쿼리 파라미터 처리, 공통 url 생성 함수 생성

--- src/main/java/kakao/rebit/book/service/AladinApiService.java
+++ src/main/java/kakao/rebit/book/service/AladinApiService.java
@@ -9,18 +9,22 @@ public class AladinApiService {
 
     private static final String API_URL = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
     private static final String API_KEY = "ttbtommy452311545001";  // 알라딘 TTBKey
+    private static final String API_QUERY_PARAMS = "&QueryType=%s&MaxResults=%s&start=1&SearchTarget=Book&output=js&Version=20131101";
+
+    private String getApiUrlByQuery(String query, String queryType, Integer maxResults) {
+        return API_URL + "?ttbkey=" + API_KEY + "&Query=" + query
+            + String.format(API_QUERY_PARAMS, queryType, maxResults);
+    }
 
     public String searchBookByTitle(String title) {
-        String url = API_URL + "?ttbkey=" + API_KEY + "&Query=" + title
-            + "&QueryType=Title&MaxResults=10&start=1&SearchTarget=Book&output=js&Version=20131101";
+        String url = getApiUrlByQuery(title, "Title", 10);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         return response.getBody();
     }
 
     public String searchBookByIsbn(String isbn) {
-        String url = API_URL + "?ttbkey=" + API_KEY + "&Query=" + isbn
-            + "&QueryType=ISBN&MaxResults=1&start=1&SearchTarget=Book&output=js&Version=20131101";
+        String url = getApiUrlByQuery(isbn, "ISBN", 1);
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         return response.getBody();
