787c372d5e153ae9d15ccbfd8dff970716f1bb0a
kangbeom <103165796+kangboom@users.noreply.github.com>
2024-09-27 19:06:50 +0900

    [FEAT] 피드 조회 구현 (#24)
    * feat: 피드 응답을 위한 BookResponse DTO 구현
    
    * feat: 인생책 컨트롤러 구현
    
    * feat: 인생책 서비스 구현
    
    * feat: 피드 컨트롤러 구현
    
    * feat: 피드 서비스 구현
    
    * feat: 메거진 컨트롤러 구현
    
    * feat: 스토리 컨트롤러 구현
    
    * feat: 스토리 서비스 구현
    
    * feat: 피드 타입 값을 나타내는 enum 클래스 구현
    
    * feat: 메거진 서비스 구현
    
    * refactor: 메거진 레파지터리 JpaRepository 상속
    
    * feat: 스토리 응답을 위한 DTO 구현
    
    * feat: 피드 응답을 위한 DTO 구현
    
    * feat: 메거진 응답을 위한 DTO 구현
    
    * feat: 인생책 응답을 위한 DTO 구현
    
    * refactor: Book 기본키 수정에 따른 변화 반영
    
    * refactor: 불필요한 클래스 삭제
    
    * refactor: type DB에서 받아오도록 수정
    
    * style: 코드 리포맷팅
    
    * feat: 구분 컬럼을 받기 위한 필드 추가
    
    * refactor: 기본키 변경
    
    * refactor: 피드전용 회원 정보 변환 DTO 구현
    
    * refactor: feed 하위 항목 엔드포인트 수정
    
    * refactor: 불필요한 어노테이션 생략
    
    * refactor: 중복되는 코드 삭제
    
    * refactor: 중복되는 DTO 변환 코드 재사용을 위한 수정
    
    * refactor: 회원 정보 DTO 의미있는 식별자로 수정

--- src/main/java/kakao/rebit/feed/repository/FeedRepository.java
+++ src/main/java/kakao/rebit/feed/repository/FeedRepository.java
@@ -2,9 +2,7 @@ package kakao.rebit.feed.repository;
 
 import kakao.rebit.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
 
 }
