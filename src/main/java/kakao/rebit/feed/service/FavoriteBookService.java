0e983d27e2b8436ff8d3f633d86e0e4aab968b46
Kangboom <jkl6147@naver.com>
2024-09-29 22:57:55 +0900

    refactor: Mapper에서 변환 로직 수행하도록 수정

--- src/main/java/kakao/rebit/feed/service/FavoriteBookService.java
+++ src/main/java/kakao/rebit/feed/service/FavoriteBookService.java
@@ -2,6 +2,7 @@ package kakao.rebit.feed.service;
 
 import kakao.rebit.feed.dto.response.FavoriteBookResponse;
 import kakao.rebit.feed.entity.FavoriteBook;
+import kakao.rebit.feed.mapper.FeedMapper;
 import kakao.rebit.feed.repository.FavoriteBookRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -12,37 +13,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class FavoriteBookService {
 
     private final FavoriteBookRepository favoriteBookRepository;
-    private final FeedService feedService;
+    private final FeedMapper feedMapper;
 
     public FavoriteBookService(FavoriteBookRepository favoriteBookRepository,
-            FeedService feedService) {
+            FeedMapper feedMapper) {
         this.favoriteBookRepository = favoriteBookRepository;
-        this.feedService = feedService;
+        this.feedMapper = feedMapper;
     }
 
     @Transactional(readOnly = true)
     public Page<FavoriteBookResponse> getFavoriteBooks(Pageable pageable) {
         Page<FavoriteBook> favorites = favoriteBookRepository.findAll(pageable);
         System.out.println(favorites);
-        return favorites.map(this::toFavoriteBookResponse);
+        return favorites.map(feedMapper::toFavoriteBookResponse);
     }
 
     @Transactional(readOnly = true)
     public FavoriteBookResponse getFavoriteBookById(Long id) {
         FavoriteBook favoriteBook = favoriteBookRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("찾는 인생책이 없습니다."));
-        return toFavoriteBookResponse(favoriteBook);
-    }
-
-    /**
-     * 아래부터는 DTO 변환 로직
-     */
-
-    private FavoriteBookResponse toFavoriteBookResponse(FavoriteBook favoriteBook) {
-        return new FavoriteBookResponse(favoriteBook.getId(),
-                feedService.toAuthorResponse(favoriteBook.getMember()),
-                feedService.toBookResponse(favoriteBook.getBook()),
-                favoriteBook.getType(), favoriteBook.getBriefReview(),
-                favoriteBook.getFullReview());
+        return feedMapper.toFavoriteBookResponse(favoriteBook);
     }
 }
