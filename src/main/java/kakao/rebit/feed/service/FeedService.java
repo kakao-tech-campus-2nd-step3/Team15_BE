a4c2825cc8be5dad068476660967ca9fecc77f5d
Kangboom <jkl6147@naver.com>
2024-09-29 23:04:25 +0900

    feat: Feed CRUD 구현 및 Mapper로 변환하도록 수정

--- src/main/java/kakao/rebit/feed/service/FeedService.java
+++ src/main/java/kakao/rebit/feed/service/FeedService.java
@@ -1,18 +1,14 @@
 package kakao.rebit.feed.service;
 
-import kakao.rebit.book.entity.Book;
-import kakao.rebit.feed.dto.response.AuthorResponse;
-import kakao.rebit.feed.dto.response.BookResponse;
-import kakao.rebit.feed.dto.response.FavoriteBookResponse;
+import kakao.rebit.feed.dto.request.create.CreateFeedRequest;
+import kakao.rebit.feed.dto.request.update.UpdateFeedRequest;
 import kakao.rebit.feed.dto.response.FeedResponse;
-import kakao.rebit.feed.dto.response.MagazineResponse;
-import kakao.rebit.feed.dto.response.StoryResponse;
-import kakao.rebit.feed.entity.FavoriteBook;
 import kakao.rebit.feed.entity.Feed;
-import kakao.rebit.feed.entity.Magazine;
-import kakao.rebit.feed.entity.Story;
+import kakao.rebit.feed.mapper.FeedMapper;
 import kakao.rebit.feed.repository.FeedRepository;
+import kakao.rebit.member.dto.MemberResponse;
 import kakao.rebit.member.entity.Member;
+import kakao.rebit.member.service.MemberService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,47 +18,49 @@ import org.springframework.transaction.annotation.Transactional;
 public class FeedService {
 
     private final FeedRepository feedRepository;
+    private final MemberService memberService;
 
-    public FeedService(FeedRepository feedRepository) {
+    private final FeedMapper feedMapper;
+
+    public FeedService(FeedRepository feedRepository, MemberService memberService,
+            FeedMapper feedMapper) {
         this.feedRepository = feedRepository;
+        this.memberService = memberService;
+        this.feedMapper = feedMapper;
     }
 
     @Transactional(readOnly = true)
     public Page<FeedResponse> getFeeds(Pageable pageable) {
-        return feedRepository.findAll(pageable).map(this::toFeedResponse);
+        return feedRepository.findAll(pageable).map(feedMapper::toFeedResponse);
     }
 
-    /**
-     * 아래부터는 DTO 변환 로직
-     */
-
-    private FeedResponse toFeedResponse(Feed feed) {
-        if (feed instanceof FavoriteBook) {
-            return new FavoriteBookResponse(feed.getId(), toAuthorResponse(feed.getMember()),
-                    toBookResponse(feed.getBook()), feed.getType(),
-                    ((FavoriteBook) feed).getBriefReview(), ((FavoriteBook) feed).getFullReview());
-        }
-        if (feed instanceof Magazine) {
-            return new MagazineResponse(feed.getId(), toAuthorResponse(feed.getMember()),
-                    toBookResponse(feed.getBook()), feed.getType(),
-                    ((Magazine) feed).getName(), ((Magazine) feed).getImageUrl(),
-                    ((Magazine) feed).getContent());
-        }
-        if (feed instanceof Story) {
-            return new StoryResponse(feed.getId(), toAuthorResponse(feed.getMember()),
-                    toBookResponse(feed.getBook()), feed.getType(),
-                    ((Story) feed).getImageUrl(), ((Story) feed).getContent());
-        }
-        throw new IllegalStateException("유효하지 않는 피드입니다.");
+    @Transactional(readOnly = true)
+    public FeedResponse getFeedById(Long feedId) {
+        Feed feed = feedRepository.findById(feedId)
+                .orElseThrow(() -> new IllegalArgumentException("찾는 피드가 존재하지 않습니다."));
+        return feedMapper.toFeedResponse(feed);
     }
 
-
-    public AuthorResponse toAuthorResponse(Member member) {
-        return new AuthorResponse(member.getId(), member.getNickname(), member.getImageUrl());
+    @Transactional
+    public Long createFeed(MemberResponse memberResponse, CreateFeedRequest feedRequest) {
+        Member member = memberService.findMemberByIdOrThrow(memberResponse.id());
+        Feed feed = feedMapper.toFeed(member, feedRequest);
+        return feedRepository.save(feed).getId();
     }
 
-    public BookResponse toBookResponse(Book book) {
-        return new BookResponse(book.getId(), book.getIsbn(), book.getTitle(),
-                book.getDescription(), book.getAuthor(), book.getPublisher(), book.getImageUrl());
+    @Transactional
+    public void updateFeed(MemberResponse memberResponse, Long feedId,
+            UpdateFeedRequest feedRequest) {
+        Member member = memberService.findMemberByIdOrThrow(memberResponse.id());
+        Feed feed = feedRepository.findByIdAndMember(feedId, member)
+                .orElseThrow(() -> new IllegalArgumentException("찾는 피드가 없습니다."));
+
+        Feed updateFeed = feedMapper.toFeed(member, feedRequest);
+        feed.updateNonNullFields(updateFeed);
+    }
+
+    @Transactional
+    public void deleteFeedById(Long feedId) {
+        feedRepository.deleteById(feedId);
     }
 }
