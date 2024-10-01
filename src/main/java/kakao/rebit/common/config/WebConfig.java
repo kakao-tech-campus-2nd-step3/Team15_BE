b1d8081771bfbcb203d187d756a8d88da4832020
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-01 18:04:47 +0900

    refactor : auth의 config 패키지를common으로 이동

--- src/main/java/kakao/rebit/common/config/WebConfig.java
+++ src/main/java/kakao/rebit/common/config/WebConfig.java
@@ -1,4 +1,4 @@
-package kakao.rebit.auth.config;
+package kakao.rebit.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
