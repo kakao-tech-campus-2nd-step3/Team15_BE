16b1ae58582bf204f9822566b370cdb0c557ad4a
Profile-exe <kang67572346@gmail.com>
2024-10-02 11:11:49 +0900

    feat: 챌린지 참여 정보 조회와 챌린지 참여 여부 확인 메서드 구현

--- src/main/java/kakao/rebit/challenge/repository/ChallengeParticipantRepository.java
+++ src/main/java/kakao/rebit/challenge/repository/ChallengeParticipantRepository.java
@@ -1,8 +1,18 @@
 package kakao.rebit.challenge.repository;
 
+import kakao.rebit.challenge.entity.Challenge;
 import kakao.rebit.challenge.entity.ChallengeParticipant;
+import kakao.rebit.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChallengeParticipantRepository extends JpaRepository<ChallengeParticipant, Long> {
 
+    @Query("SELECT cp FROM ChallengeParticipant cp JOIN FETCH cp.member JOIN FETCH cp.challenge WHERE cp.challenge = :challenge")
+    Page<ChallengeParticipant> findAllByChallenge(@Param("challenge") Challenge challenge, Pageable pageable);
+
+    Boolean existsByMemberAndChallenge(Member member, Challenge challenge);
 }
