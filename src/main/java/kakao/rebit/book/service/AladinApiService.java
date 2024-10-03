f589daf037cdd93ec359b10792573543f000d2b5
안원모 <tommy45231@naver.com>
2024-10-03 15:09:26 +0900

    refactor: 분리된 URL 빌더 메서드 로직 수정

--- src/main/java/kakao/rebit/book/service/AladinApiService.java
+++ src/main/java/kakao/rebit/book/service/AladinApiService.java
@@ -7,13 +7,26 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class AladinApiService {
 
-    private static final String API_URL = "http://www.aladin.co.kr/ttb/api/ItemSearch.aspx";
-    private static final String API_KEY = "ttbtommy452311545001";  // 알라딘 TTBKey
-    private static final String API_QUERY_PARAMS = "&QueryType=%s&MaxResults=%s&start=1&SearchTarget=Book&output=js&Version=20131101";
+    private static final String BASE_API_URL = "http://www.aladin.co.kr/ttb/api";
+    private static final String TTB_KEY = "ttbtommy452311545001";  // 알라딘 TTBKey
+    private static final String QUERY_PARAMS_FORMAT = "&QueryType=%s&MaxResults=%s&start=1&SearchTarget=Book&output=js&Version=20131101";
+    private static final String ITEM_LOOKUP_ENDPOINT = "/ItemLookUp.aspx";
+    private static final String ITEM_SEARCH_ENDPOINT = "/ItemSearch.aspx";
 
-    private String getApiUrlByQuery(String query, String queryType, Integer maxResults) {
-        return API_URL + "?ttbkey=" + API_KEY + "&Query=" + query
-            + String.format(API_QUERY_PARAMS, queryType, maxResults);
+
+    private String buildTitleSearchUrl(String title, int maxResults) {
+        return BASE_API_URL + ITEM_SEARCH_ENDPOINT
+            + "?ttbkey=" + TTB_KEY
+            + "&Query=" + title
+            + String.format(QUERY_PARAMS_FORMAT, "Title", maxResults);
+    }
+
+    private String buildIsbnLookupUrl(String isbn) {
+        return BASE_API_URL + ITEM_LOOKUP_ENDPOINT
+            + "?ttbkey=" + TTB_KEY
+            + "&itemIdType=ISBN"
+            + "&ItemId=" + isbn
+            + "&output=js&Version=20131101";
     }
 
     private String sendRequest(String query, String queryType, int maxResults) {
