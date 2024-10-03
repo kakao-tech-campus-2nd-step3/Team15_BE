5c7121359a23e6dab2cc6417720b447c533f1362
Profile-exe <kang67572346@gmail.com>
2024-10-03 23:43:26 +0900

    refactor: ChallengeVerificationRepository로 이름 변경

---
+++ src/main/java/kakao/rebit/challenge/repository/ChallengeVerificationRepository.java
@@ -0,0 +1,8 @@
+package kakao.rebit.challenge.repository;
+
+import kakao.rebit.challenge.entity.ChallengeVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChallengeVerificationRepository extends JpaRepository<ChallengeVerification, Long> {
+
+}
