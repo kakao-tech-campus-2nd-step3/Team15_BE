aa53cd5f20444071f72ad9667da35295ee294728
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-04 01:24:56 +0900

    feat : 사용자 조회 수정 삭제 로직 추가

--- src/main/java/kakao/rebit/member/service/MemberService.java
+++ src/main/java/kakao/rebit/member/service/MemberService.java
@@ -1,9 +1,12 @@
 package kakao.rebit.member.service;
 
+import kakao.rebit.member.dto.MemberRequest;
 import kakao.rebit.member.entity.Member;
 import kakao.rebit.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -15,8 +18,45 @@ public class MemberService {
     }
 
     @Transactional(readOnly = true)
+    public List<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
     public Member findMemberByIdOrThrow(Long memberId) {
         return memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+            .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public Member findMemberByEmail(String email) {
+        return memberRepository.findByEmail(email)
+            .orElseThrow(
+                () -> new IllegalArgumentException("Member with email " + email + " not found"));
+    }
+
+    @Transactional
+    public Member updateMember(Long memberId, MemberRequest memberRequest) {
+        Member member = findMemberByIdOrThrow(memberId);
+        member.setNickname(memberRequest.getNickname());
+        member.setBio(memberRequest.getBio());
+        member.setImageUrl(memberRequest.getImageUrl());
+        member.setPoint(memberRequest.getPoint());
+        return memberRepository.save(member);
+    }
+
+    @Transactional
+    public Member updateMyMember(String email, MemberRequest memberRequest) {
+        Member member = findMemberByEmail(email);
+        member.setNickname(memberRequest.getNickname());
+        member.setBio(memberRequest.getBio());
+        member.setImageUrl(memberRequest.getImageUrl());
+        return memberRepository.save(member);
+    }
+
+    @Transactional
+    public void deleteMember(Long memberId) {
+        Member member = findMemberByIdOrThrow(memberId);
+        memberRepository.delete(member);
     }
 }
