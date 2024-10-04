998772d877cfb5a618fb9a28c71683f3201f6bab
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-04 22:43:02 +0900

    refactor : 문자열 리터럴을 상수변수로 관리하도록 수정

--- src/main/java/kakao/rebit/auth/jwt/JwtInterceptor.java
+++ src/main/java/kakao/rebit/auth/jwt/JwtInterceptor.java
@@ -10,14 +10,17 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
 
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String token = request.getHeader("Authorization");
-        if (token != null && token.startsWith("Bearer ")) {
-            token = token.substring(7);  // Bearer 제거
+        String token = request.getHeader(AUTHORIZATION_HEADER);
+        if (token != null && token.startsWith(BEARER_PREFIX)) {
+            token = token.substring(BEARER_PREFIX.length());  // Bearer 제거
             if (jwtTokenProvider.validateToken(token)) {
                 String email = jwtTokenProvider.getEmailFromToken(token);
                 String role = jwtTokenProvider.getRoleFromToken(token);
