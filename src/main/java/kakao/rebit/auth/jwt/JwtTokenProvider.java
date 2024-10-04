9ab88a4fcdcd28f7f25195807a6d42d0750d3027
설빈 <138611481+seolbin96@users.noreply.github.com>
2024-10-04 23:45:46 +0900

    [FEAT] 사용자 조회, 수정, 삭제 기능 구현 (#53)
    * refactor : token 패키지명 첫글자를 대문자에서 소문자로 수정
    
    * fix : email 파라미터순서 오입력 수정 및 null값을 빈 스트링으로 수정
    
    * refactor : 숫자 리터럴에 언더바 추가
    
    * refactor : auth의 config 패키지를common으로 이동
    
    * refactor : SnakeCaseStrategy 적용
    
    * feat : Member 클래스에 setter 메서드 추가
    
    * feat : Jwt 토큰 생성 시 역할 정보 추가
    
    * feat : Jwt 토큰에서 이메일 및 역할 추출 메서드 추가
    
    * feat : AuthTokenGenerator 클래스 수정에 따라 역할 정보 추가
    
    * feat: 사용자 정보 전달을 위한 MemberInfo, MemberInfoDto, MemberRequest 클래스 추가
    
    * feat : Jwt 인터셉터 클래스 추가
    
    * feat : ArgumentResolver 클래스 추가
    
    * feat : 사용자 조회 수정 삭제를 위한 Member 컨트롤러 구현
    
    * feat : 사용자 조회 수정 삭제 로직 추가
    
    * feat: 인터셉터 및 ArgumentResolver 등록
    
    * feat : ChargePointRequest dto 추가
    
    * feat : 포인트 조회 및 충전 기능 구현
    
    * feat : dto를 record 사용하도록 수정
    
    * refactor : setter 대신 의미있는 메서드 사용
    
    * feat : @Transactional 어노테이션 추가 및 메서드명 수정
    
    * refactor : 문자열 리터럴을 상수변수로 관리하도록 수정
    
    * refactor : 메서드 분리
    
    * feat : email 추가
    
    * refactor : 메서드명 수정
    
    * refactor : memberService를 이용해 이메일로 회원 정보 조회

--- src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
+++ src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
@@ -1,8 +1,11 @@
 package kakao.rebit.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,9 +20,11 @@ public class JwtTokenProvider {
         this.key = Keys.hmacShaKeyFor(java.util.Base64.getDecoder().decode(base64Secret));
     }
 
-    public String accessTokenGenerate(String uid, Date expiryDate) {
+    public String accessTokenGenerate(String uid, String email, String role, Date expiryDate) {
         return Jwts.builder()
             .setSubject(uid)
+            .claim("email", email)
+            .claim("role", role)
             .setExpiration(expiryDate)
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
@@ -30,5 +35,38 @@ public class JwtTokenProvider {
             .setExpiration(expiryDate)
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
+    }
+
+    // 토큰에서 이메일 추출
+    public String getEmailFromToken(String token) {
+        return extractClaim(token).get("email", String.class);
+    }
+
+    // 토큰에서 역할 추출
+    public String getRoleFromToken(String token) {
+        return extractClaim(token).get("role", String.class);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            System.out.println("유효하지 않은 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            System.out.println("만료된 JWT 토큰입니다.");
+        } catch (Exception e) {
+            System.out.println("유효하지 않은 JWT 토큰입니다.");
+        }
+        return false;
+    }
+
+    // 토큰에서 클레임 정보 추출
+    private Claims extractClaim(String token) {
+        return Jwts.parser()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 }
--- src/main/java/kakao/rebit/auth/token/AuthTokenGenerator.java
+++ src/main/java/kakao/rebit/auth/token/AuthTokenGenerator.java
@@ -1,4 +1,4 @@
-package kakao.rebit.auth.Token;
+package kakao.rebit.auth.token;
 
 import kakao.rebit.auth.jwt.JwtTokenProvider;
 import org.springframework.stereotype.Component;
@@ -7,8 +7,8 @@ import java.util.Date;
 @Component
 public class AuthTokenGenerator {
 
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 3600000;
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1209600000;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 3_600_000;
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1_209_600_000;
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -16,14 +16,15 @@ public class AuthTokenGenerator {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    public AuthToken generate(String uid) {
+    public AuthToken generate(String uid, String email, String role) {
         Date now = new Date();
         Date accessTokenExpiry = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
         Date refreshTokenExpiry = new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME);
 
-        String accessToken = jwtTokenProvider.accessTokenGenerate(uid, accessTokenExpiry);
+        String accessToken = jwtTokenProvider.accessTokenGenerate(uid, email, role, accessTokenExpiry);
         String refreshToken = jwtTokenProvider.refreshTokenGenerate(refreshTokenExpiry);
 
         return AuthToken.of(accessToken, refreshToken, "Bearer", ACCESS_TOKEN_EXPIRE_TIME / 1000);
     }
+
 }
