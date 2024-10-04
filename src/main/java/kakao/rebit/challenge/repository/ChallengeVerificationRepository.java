fa62d3c5a0d2b22593415d59972e6a85d680fca7
Profile-exe <kang67572346@gmail.com>
2024-10-04 16:43:43 +0900

    feat: 챌린지 인증글 리파지토리 조회 메서드 구현

--- src/main/java/kakao/rebit/challenge/repository/ChallengeVerificationRepository.java
+++ src/main/java/kakao/rebit/challenge/repository/ChallengeVerificationRepository.java
@@ -1,8 +1,21 @@
 package kakao.rebit.challenge.repository;
 
+import java.util.Optional;
+import kakao.rebit.challenge.entity.Challenge;
 import kakao.rebit.challenge.entity.ChallengeVerification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ChallengeVerificationRepository extends JpaRepository<ChallengeVerification, Long> {
 
+    @EntityGraph(attributePaths = {"challengeParticipation", "challengeParticipation.member", "challengeParticipation.challenge"})
+    @Query("SELECT cv FROM ChallengeVerification cv WHERE cv.challengeParticipation.challenge = :challenge")
+    Page<ChallengeVerification> findAllByChallengeWithParticipants(@Param("challenge") Challenge challenge, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"challengeParticipation", "challengeParticipation.member", "challengeParticipation.challenge"})
+    Optional<ChallengeVerification> findByIdAndChallengeParticipation_Challenge(Long verificationId, Challenge challenge);
 }
