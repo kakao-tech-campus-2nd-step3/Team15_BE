746fa8b67df6340452aeb44f31aa5533ba67ad2b
안원모 <tommy45231@naver.com>
2024-10-04 12:21:19 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/java/kakao/rebit/feed/dto/response/BookResponse.java
+++ src/main/java/kakao/rebit/feed/dto/response/BookResponse.java
@@ -1,17 +1,14 @@
 package kakao.rebit.feed.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-@JsonPropertyOrder({ "id", "isbn", "title", "author", "cover", "description", "publisher", "pubDate" })
 public record BookResponse(
-        Long id,
-        String isbn,
-        String title,
-        String author,
-        String cover,
-        String description,
-        String publisher,
-        String pubDate
+    Long id,
+    String isbn,
+    String title,
+    String author,
+    String cover,
+    String description,
+    String publisher,
+    String pubDate
 ) {
 
 }
