8c1071f24d4df2ee4f1858be6739adb8e940fce5
Kangboom <jkl6147@naver.com>
2024-10-05 17:30:41 +0900

    refactor: resolver를 통해 MemberResponse 받도록 수정

--- src/main/java/kakao/rebit/challenge/controller/ChallengeController.java
+++ src/main/java/kakao/rebit/challenge/controller/ChallengeController.java
@@ -4,8 +4,8 @@ import java.net.URI;
 import kakao.rebit.challenge.dto.ChallengeRequest;
 import kakao.rebit.challenge.dto.ChallengeResponse;
 import kakao.rebit.challenge.service.ChallengeService;
+import kakao.rebit.member.annotation.MemberInfo;
 import kakao.rebit.member.dto.MemberResponse;
-import kakao.rebit.member.entity.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -41,18 +41,14 @@ public class ChallengeController {
     }
 
     @PostMapping
-    public ResponseEntity<Void> createChallenge(@RequestBody ChallengeRequest challengeRequest) {
-        // 소셜 로그인이 아직 구현되지 않아 임의의 빈 MemberResponse를 넣어줌
-        // 추후에 소셜 로그인 구현 후 ArgumentResolver로 MemberResponse를 받아오도록 수정
-        MemberResponse memberResponse = new MemberResponse(1L, "testUser", "imageUrl", "bio", Role.ROLE_USER, 10000);
+    public ResponseEntity<Void> createChallenge(@MemberInfo MemberResponse memberResponse, @RequestBody ChallengeRequest challengeRequest) {
         Long challengeId = challengeService.createChallenge(memberResponse, challengeRequest);
         return ResponseEntity.created(URI.create("/challenges/" + challengeId)).build();
     }
 
     @DeleteMapping("/{challenge-id}")
-    public ResponseEntity<Void> deleteChallenge(@PathVariable("challenge-id") Long challengeId) {
-        MemberResponse memberResponse = new MemberResponse(1L, "testUser", "imageUrl", "bio", Role.ROLE_USER, 10000);
-        challengeService.deleteChallengeById(memberResponse, challengeId);
+    public ResponseEntity<Void> deleteChallenge(@MemberInfo MemberResponse memberResponse, @PathVariable("challenge-id") Long challengeId) {
+       challengeService.deleteChallengeById(memberResponse, challengeId);
         return ResponseEntity.noContent().build();
     }
 }
