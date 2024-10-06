4088eca41f9a8307ebede7524f916e48d0c0cc71
Kangboom <jkl6147@naver.com>
2024-10-06 23:06:37 +0900

    feat: request dto 검증 구현

--- src/main/java/kakao/rebit/feed/dto/request/create/CreateStoryRequest.java
+++ src/main/java/kakao/rebit/feed/dto/request/create/CreateStoryRequest.java
@@ -1,8 +1,13 @@
 package kakao.rebit.feed.dto.request.create;
 
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
+
 public class CreateStoryRequest extends CreateFeedRequest {
 
+    @URL(message = "잘못된 URL 형식입니다.")
     private String imageUrl;
+    @NotBlank(message = "본문은 필수 입력 값입니다.")
     private String content;
 
     private CreateStoryRequest() {
--- src/main/java/kakao/rebit/feed/dto/request/update/UpdateFavoriteBookRequest.java
+++ src/main/java/kakao/rebit/feed/dto/request/update/UpdateFavoriteBookRequest.java
@@ -1,9 +1,12 @@
 package kakao.rebit.feed.dto.request.update;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class UpdateFavoriteBookRequest extends UpdateFeedRequest {
 
+    @NotBlank(message = "한줄평은 필수 입력 값입니다.")
     private String briefReview;
-
+    @NotBlank(message = "서평은 필수 입력 값입니다.")
     private String fullReview;
 
     private UpdateFavoriteBookRequest() {
