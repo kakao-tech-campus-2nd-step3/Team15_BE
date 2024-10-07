cbcd0d3c3c7c62de93f7ad8021685d9668fe1a39
Profile-exe <kang67572346@gmail.com>
2024-10-07 22:15:23 +0900

    feat: 특정 엔드포인트를 검증 로직에서 제외하는 어노테이션 구현

--- src/main/java/kakao/rebit/auth/jwt/JwtInterceptor.java
+++ src/main/java/kakao/rebit/auth/jwt/JwtInterceptor.java
@@ -2,9 +2,10 @@ package kakao.rebit.auth.jwt;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import kakao.rebit.common.annotation.AllowAnonymous;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
@@ -24,6 +25,11 @@ public class JwtInterceptor implements HandlerInterceptor {
 
         // CORS preflight 요청은 토큰 검증을 하지 않음
         if (CorsUtils.isPreFlightRequest(request)) {
+            return true;
+        }
+
+        // AllowAnonymous 어노테이션이 붙어있는 경우 토큰 검증을 하지 않음
+        if (handler instanceof HandlerMethod handlerMethod && handlerMethod.getMethodAnnotation(AllowAnonymous.class) != null) {
             return true;
         }
 
