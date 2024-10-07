c66d2e9ed81d2737d17247f17c3365bbcbefcc76
Profile-exe <kang67572346@gmail.com>
2024-10-07 11:19:36 +0900

    feat: 챌린지 비즈니스 예외에 대한 에러코드 구현

---
+++ src/main/java/kakao/rebit/challenge/exception/challenge/ChallengeErrorCode.java
@@ -0,0 +1,36 @@
+package kakao.rebit.challenge.exception.challenge;
+
+import kakao.rebit.common.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+public enum ChallengeErrorCode implements ErrorCode {
+    CHALLENGE_NOT_FOUND("CH001", HttpStatus.NOT_FOUND, "챌린지를 찾을 수 없습니다."),
+    CHALLENGE_DELETE_NOT_AUTHORIZED("CH002", HttpStatus.FORBIDDEN, "챌린지 주최자만 삭제할 수 있습니다."),
+    CHALLENGE_DELETE_NOT_ALLOWED("CH003", HttpStatus.BAD_REQUEST, "진행 중이거나 종료된 챌린지는 삭제할 수 없습니다."),
+    ;
+
+    private final String code;
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    ChallengeErrorCode(String code, HttpStatus httpStatus, String message) {
+        this.code = code;
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
