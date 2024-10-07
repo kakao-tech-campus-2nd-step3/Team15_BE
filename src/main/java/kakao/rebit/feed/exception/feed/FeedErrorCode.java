5c3695936c95bc43d5d125661f6a2928a0d8fab1
Kangboom <jkl6147@naver.com>
2024-10-07 22:27:19 +0900

    style: 코드 리포맷팅

--- src/main/java/kakao/rebit/feed/exception/feed/FeedErrorCode.java
+++ src/main/java/kakao/rebit/feed/exception/feed/FeedErrorCode.java
@@ -7,8 +7,7 @@ public enum FeedErrorCode implements ErrorCode {
     NOT_FOUND("F001", HttpStatus.NOT_FOUND, "피드를 찾을 수 없습니다."),
     UPDATE_NOT_AUTHORIZED("F002", HttpStatus.FORBIDDEN, "피드 작성자만 수정할 수 있습니다."),
     DELETE_NOT_AUTHORIZED("F003", HttpStatus.FORBIDDEN, "피드 작성자만 삭제할 수 있습니다."),
-    FAVORITE_BOOK_REQUIRED_BOOK("F004", HttpStatus.BAD_REQUEST, "인생책 피드는 책이 반드시 필요합니다.")
-    ;
+    FAVORITE_BOOK_REQUIRED_BOOK("F004", HttpStatus.BAD_REQUEST, "인생책 피드는 책이 반드시 필요합니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
--- src/main/java/kakao/rebit/feed/exception/likes/LikesErrorCode.java
+++ src/main/java/kakao/rebit/feed/exception/likes/LikesErrorCode.java
@@ -6,8 +6,7 @@ import org.springframework.http.HttpStatus;
 public enum LikesErrorCode implements ErrorCode {
     LIKES_ALREADY_PRESSED("L001", HttpStatus.BAD_REQUEST, "이미 좋아요를 누른 상태입니다."),
     LIKES_NOT_PRESSED("L002", HttpStatus.BAD_REQUEST, "좋아요를 누르지 않은 상태입니다."),
-    FIND_NOT_AUTHORIZED("L003", HttpStatus.FORBIDDEN, "피드 작성자만 좋아요 목록을 확인할 수 있습니다.")
-    ;
+    FIND_NOT_AUTHORIZED("L003", HttpStatus.FORBIDDEN, "피드 작성자만 좋아요 목록을 확인할 수 있습니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
