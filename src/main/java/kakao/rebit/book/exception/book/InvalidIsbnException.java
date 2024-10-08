2461969a93cf3ceeff77cf4f8e0a843557ab45f4
안원모 <tommy45231@naver.com>
2024-10-08 20:58:15 +0900

    style: 코드 리포맷팅

--- src/main/java/kakao/rebit/book/exception/book/InvalidIsbnException.java
+++ src/main/java/kakao/rebit/book/exception/book/InvalidIsbnException.java
@@ -3,6 +3,7 @@ package kakao.rebit.book.exception.book;
 import kakao.rebit.common.exception.BusinessException;
 
 public class InvalidIsbnException extends BusinessException {
+
     public static final BusinessException EXCEPTION = new InvalidIsbnException();
 
     private InvalidIsbnException() {
