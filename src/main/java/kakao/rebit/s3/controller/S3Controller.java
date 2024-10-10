e7ae671e2f9dae89fe99f8f07d9ff88b4b188ca6
Kangboom <jkl6147@naver.com>
2024-10-11 00:15:39 +0900

    refactor: @AllowAnonymous 제거

--- src/main/java/kakao/rebit/s3/controller/S3Controller.java
+++ src/main/java/kakao/rebit/s3/controller/S3Controller.java
@@ -1,7 +1,7 @@
 package kakao.rebit.s3.controller;
 
 import kakao.rebit.common.annotation.AllowAnonymous;
-import kakao.rebit.s3.dto.UploadResponse;
+import kakao.rebit.s3.dto.S3UploadUrlResponse;
 import kakao.rebit.s3.service.S3Service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +19,10 @@ public class S3Controller {
         this.s3Service = s3Service;
     }
 
-    @AllowAnonymous
     @GetMapping("/upload")
-    public ResponseEntity<UploadResponse> getUploadS3Url(
+    public ResponseEntity<S3UploadUrlResponse> getUploadS3Url(
             @RequestParam("filename") String filename) {
-        UploadResponse s3UrlDto = s3Service.getUploadUrl(filename);
+        S3UploadUrlResponse s3UrlDto = s3Service.getUploadUrl(filename);
         return ResponseEntity.ok().body(s3UrlDto);
     }
 }
