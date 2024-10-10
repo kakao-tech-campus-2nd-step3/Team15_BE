cf2f761b066e70be81a13146422725e89fafb9e7
Kangboom <jkl6147@naver.com>
2024-10-11 01:42:01 +0900

    feat: imageKey 검증 패턴 추가

--- src/main/java/kakao/rebit/feed/dto/request/create/CreateStoryRequest.java
+++ src/main/java/kakao/rebit/feed/dto/request/create/CreateStoryRequest.java
@@ -1,10 +1,12 @@
 package kakao.rebit.feed.dto.request.create;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import kakao.rebit.common.domain.ImageKeyHolder;
 
 public class CreateStoryRequest extends CreateFeedRequest {
 
-    @NotBlank(message = "이미지는 필수 입니다.")
+    @Pattern(regexp = ImageKeyHolder.IMAGE_KEY_FORMAT, message = "피드 imageKey는 'feed/%s/%s' 형식이어야 합니다.")
     private String imageKey;
     @NotBlank(message = "본문은 필수 입력 값입니다.")
     private String content;
