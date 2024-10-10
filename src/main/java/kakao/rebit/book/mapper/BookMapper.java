2e401312402a07307e6d269601c7ab611dfe9031
안원모 <tommy45231@naver.com>
2024-10-10 20:48:18 +0900

    feat: 한줄평,서평 포함 응답 반환 구현

--- src/main/java/kakao/rebit/book/mapper/BookMapper.java
+++ src/main/java/kakao/rebit/book/mapper/BookMapper.java
@@ -1,8 +1,10 @@
 package kakao.rebit.book.mapper;
 
 import kakao.rebit.book.dto.AladinApiResponseResponse;
+import kakao.rebit.book.dto.BookDetailResponse;
 import kakao.rebit.book.dto.BookResponse;
 import kakao.rebit.book.entity.Book;
+import kakao.rebit.feed.entity.FavoriteBook;
 
 public class BookMapper {
 
@@ -28,6 +30,26 @@ public class BookMapper {
             response.publisher(),
             response.cover(),
             response.pubDate()
+        );
+    }
+
+    public static BookDetailResponse toBookDetailResponse(Book book, FavoriteBook topFavoriteBook) {
+        String briefReview =
+            topFavoriteBook != null ? topFavoriteBook.getBriefReview() : "한줄평이 없습니다.";
+        String topFullReview =
+            topFavoriteBook != null ? topFavoriteBook.getFullReview() : "서평이 없습니다.";
+
+        return new BookDetailResponse(
+            book.getId(),
+            book.getIsbn(),
+            book.getTitle(),
+            book.getAuthor(),
+            book.getCover(),
+            book.getDescription(),
+            book.getPublisher(),
+            book.getPubDate(),
+            briefReview,
+            topFullReview
         );
     }
 }
