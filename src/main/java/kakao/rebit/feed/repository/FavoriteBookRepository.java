b442cc18b907833b0bfe9a28dba1c5ea8c4f2bb6
안원모 <tommy45231@naver.com>
2024-10-10 20:51:11 +0900

    feat: 특정 책 좋아요 순으로 인생책 조회하는 메서드 추가

--- src/main/java/kakao/rebit/feed/repository/FavoriteBookRepository.java
+++ src/main/java/kakao/rebit/feed/repository/FavoriteBookRepository.java
@@ -1,8 +1,11 @@
 package kakao.rebit.feed.repository;
 
+import java.util.Optional;
+import kakao.rebit.book.entity.Book;
 import kakao.rebit.feed.entity.FavoriteBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FavoriteBookRepository extends JpaRepository<FavoriteBook, Long> {
-
+    // 해당 책에 대해 가장 많은 좋아요를 받은 인생책 피드를 반환
+    Optional<FavoriteBook> findTopByBookOrderByLikesDesc(Book book);
 }
