2bd57d1078f7f81d9b028ac15863f2f2ed18f014
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-11 19:26:24 +0900

    feat : DiaryDto를 DiaryRequest와 DiaryResponse로 수정

--- src/main/java/kakao/rebit/diary/dto/DiaryDto.java
+++
@@ -1,3 +0,0 @@
-package kakao.rebit.diary.dto;
-
-public record DiaryDto(Long id, String content, Long memberId, String isbn) {}
---
+++ src/main/java/kakao/rebit/diary/dto/DiaryResponse.java
@@ -0,0 +1,3 @@
+package kakao.rebit.diary.dto;
+
+public record DiaryResponse(Long id, String content, Long memberId, String isbn) {}
