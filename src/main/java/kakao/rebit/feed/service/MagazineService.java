2a020a7973f036f000c2866b74588c9fe09d3209
Kangboom <jkl6147@naver.com>
2024-10-12 23:44:10 +0900

    feat: findByIdOrThrow 메서드 추가 및 update 메서드 구현

--- src/main/java/kakao/rebit/feed/service/MagazineService.java
+++ src/main/java/kakao/rebit/feed/service/MagazineService.java
@@ -1,10 +1,18 @@
 package kakao.rebit.feed.service;
 
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.book.service.BookService;
+import kakao.rebit.feed.dto.request.update.UpdateMagazineRequest;
 import kakao.rebit.feed.dto.response.FeedResponse;
 import kakao.rebit.feed.entity.Magazine;
 import kakao.rebit.feed.exception.feed.FeedNotFoundException;
+import kakao.rebit.feed.exception.feed.UpdateNotAuthorizedException;
 import kakao.rebit.feed.mapper.FeedMapper;
 import kakao.rebit.feed.repository.MagazineRepository;
+import kakao.rebit.member.dto.MemberResponse;
+import kakao.rebit.member.entity.Member;
+import kakao.rebit.member.service.MemberService;
+import kakao.rebit.s3.service.S3Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,11 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class MagazineService {
 
     private final MagazineRepository magazineRepository;
+    private final MemberService memberService;
+    private final BookService bookService;
     private final FeedMapper feedMapper;
+    private final S3Service s3Service;
 
-    public MagazineService(MagazineRepository magazineRepository, FeedMapper feedMapper) {
+    public MagazineService(MagazineRepository magazineRepository, MemberService memberService,
+            BookService bookService, FeedMapper feedMapper, S3Service s3Service) {
         this.magazineRepository = magazineRepository;
+        this.memberService = memberService;
+        this.bookService = bookService;
         this.feedMapper = feedMapper;
+        this.s3Service = s3Service;
     }
 
     @Transactional(readOnly = true)
@@ -27,9 +42,36 @@ public class MagazineService {
     }
 
     @Transactional(readOnly = true)
-    public FeedResponse getMagazineById(Long id) {
-        Magazine magazine = magazineRepository.findById(id)
-                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+    public FeedResponse getMagazineById(Long magazineId) {
+        Magazine magazine = findMagazineByIdOrThrow(magazineId);
         return feedMapper.toFeedResponse(magazine);
+    }
+
+    @Transactional(readOnly = true)
+    public Magazine findMagazineByIdOrThrow(Long magazineId) {
+        return magazineRepository.findById(magazineId)
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+    }
+
+    @Transactional
+    public void updateMagazine(MemberResponse memberResponse, Long magazineId,
+            UpdateMagazineRequest updateRequest) {
+        Member member = memberService.findMemberByIdOrThrow(memberResponse.id());
+        Magazine magazine = findMagazineByIdOrThrow(magazineId);
+
+        if (!magazine.isWrittenBy(member)) {
+            throw UpdateNotAuthorizedException.EXCEPTION;
+        }
+
+        Book book = bookService.findBookIfBookIdExist(updateRequest.bookId()).orElse(null);
+        magazine.changeBook(book);
+
+        // 이미지가 수정됐으면 기존의 S3에서 이전 이미지 삭제하기
+        if (!magazine.isImageKeyUpdated(updateRequest.imageKey())) {
+            s3Service.deleteObject(magazine.getImageKey());
+        }
+
+        magazine.changeImageKey(updateRequest.imageKey());
+        magazine.updateTextFields(updateRequest.name(), updateRequest.content());
     }
 }
--- src/main/java/kakao/rebit/feed/service/StoryService.java
+++ src/main/java/kakao/rebit/feed/service/StoryService.java
@@ -1,10 +1,18 @@
 package kakao.rebit.feed.service;
 
+import kakao.rebit.book.entity.Book;
+import kakao.rebit.book.service.BookService;
+import kakao.rebit.feed.dto.request.update.UpdateStoryRequest;
 import kakao.rebit.feed.dto.response.FeedResponse;
 import kakao.rebit.feed.entity.Story;
 import kakao.rebit.feed.exception.feed.FeedNotFoundException;
+import kakao.rebit.feed.exception.feed.UpdateNotAuthorizedException;
 import kakao.rebit.feed.mapper.FeedMapper;
 import kakao.rebit.feed.repository.StoryRepository;
+import kakao.rebit.member.dto.MemberResponse;
+import kakao.rebit.member.entity.Member;
+import kakao.rebit.member.service.MemberService;
+import kakao.rebit.s3.service.S3Service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -14,11 +22,18 @@ import org.springframework.transaction.annotation.Transactional;
 public class StoryService {
 
     private final StoryRepository storyRepository;
+    private final MemberService memberService;
+    private final BookService bookService;
     private final FeedMapper feedMapper;
+    private final S3Service s3Service;
 
-    public StoryService(StoryRepository storyRepository, FeedMapper feedMapper) {
+    public StoryService(StoryRepository storyRepository, MemberService memberService,
+            BookService bookService, FeedMapper feedMapper, S3Service s3Service) {
         this.storyRepository = storyRepository;
+        this.memberService = memberService;
+        this.bookService = bookService;
         this.feedMapper = feedMapper;
+        this.s3Service = s3Service;
     }
 
     @Transactional(readOnly = true)
@@ -27,9 +42,36 @@ public class StoryService {
     }
 
     @Transactional(readOnly = true)
-    public FeedResponse getStoryById(Long id) {
-        Story story = storyRepository.findById(id)
-                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+    public FeedResponse getStoryById(Long storyId) {
+        Story story = findStoryByIdOrThrow(storyId);
         return feedMapper.toFeedResponse(story);
+    }
+
+    @Transactional(readOnly = true)
+    public Story findStoryByIdOrThrow(Long magazineId) {
+        return storyRepository.findById(magazineId)
+                .orElseThrow(() -> FeedNotFoundException.EXCEPTION);
+    }
+
+    @Transactional
+    public void updateStory(MemberResponse memberResponse, Long storyId,
+            UpdateStoryRequest updateRequest) {
+        Member member = memberService.findMemberByIdOrThrow(memberResponse.id());
+        Story story = findStoryByIdOrThrow(storyId);
+
+        if (!story.isWrittenBy(member)) {
+            throw UpdateNotAuthorizedException.EXCEPTION;
+        }
+
+        Book book = bookService.findBookIfBookIdExist(updateRequest.bookId()).orElse(null);
+        story.changeBook(book);
+
+        // 이미지가 수정됐으면 기존의 S3에서 이전 이미지 삭제하기
+        if (!story.isImageKeyUpdated(updateRequest.imageKey())) {
+            s3Service.deleteObject(story.getImageKey());
+        }
+
+        story.changeImageKey(updateRequest.imageKey());
+        story.updateTextFields(updateRequest.content());
     }
 }
