5e3bbf8e6a9615d58fe2aa453f95d5d61b4db491
Kangboom <jkl6147@naver.com>
2024-10-12 23:41:52 +0900

    feat: 응답 필드에 imageKey 추가

--- src/main/java/kakao/rebit/feed/dto/response/StoryResponse.java
+++ src/main/java/kakao/rebit/feed/dto/response/StoryResponse.java
@@ -5,17 +5,23 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "스토리 응답")
 public class StoryResponse extends FeedResponse {
 
-    private String presignedUrl;
-    private String content;
+    private final String imageKey;
+    private final String presignedUrl;
+    private final String content;
 
-    public StoryResponse(Long id, FeedAuthorResponse author, FeedBookResponse book,
-            String type, int likes, String presignedUrl, String content) {
+    public StoryResponse(Long id, FeedAuthorResponse author, FeedBookResponse book, String type,
+            int likes, String imageKey, String presignedUrl, String content) {
         super(id, author, book, type, likes);
+        this.imageKey = imageKey;
         this.presignedUrl = presignedUrl;
         this.content = content;
     }
 
-    public String getImagePresignedUrl() {
+    public String getImageKey() {
+        return imageKey;
+    }
+
+    public String getPresignedUrl() {
         return presignedUrl;
     }
 
