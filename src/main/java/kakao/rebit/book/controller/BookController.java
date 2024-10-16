c4c8b7be969172a6ec62c4da01696f9f73977f3f
Wonmoan <102709424+Wonmoan@users.noreply.github.com>
2024-10-16 23:24:24 +0900

    [FEAT] 알라딘 API cover 화질 개선 및 책 도메인 swagger 문서화 (#95)
    * refactor: cover 이미지 반환 크기 수정
    
    * feat: 책 도메인 문서화

--- src/main/java/kakao/rebit/book/controller/BookController.java
+++ src/main/java/kakao/rebit/book/controller/BookController.java
@@ -1,5 +1,8 @@
 package kakao.rebit.book.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import kakao.rebit.book.dto.BookDetailResponse;
 import kakao.rebit.book.dto.BookResponse;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/books")
+@Tag(name = "책 API", description = "책 관련 API")
 public class BookController {
 
     private final BookService bookService;
@@ -23,19 +27,23 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Operation(summary = "전체 책 목록 조회", description = "서버에 저장된 모든 책 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<List<BookResponse>> getAllBooks() {
         List<BookResponse> books = bookService.getAllBooks();
         return ResponseEntity.ok(books);
     }
 
+    @Operation(summary = "책 제목으로 검색", description = "제목을 기준으로 책을 검색하고 결과를 페이지네이션 처리하여 반환합니다.")
     @GetMapping("/search")
     public ResponseEntity<Page<BookResponse>> searchBooksByTitle(
-        @RequestParam(name = "title") String title, Pageable pageable) {
+        @Parameter(description = "검색할 책 제목") @RequestParam(name = "title") String title,
+        Pageable pageable) {
         Page<BookResponse> books = bookService.searchAndSaveBooksByTitle(title, pageable);
         return ResponseEntity.ok(books);
     }
 
+    @Operation(summary = "책 상세 정보 조회", description = "ISBN을 기준으로 책의 상세 정보를 조회합니다.")
     @GetMapping("/{isbn}")
     public ResponseEntity<BookResponse> getBookDetail(@PathVariable(name = "isbn") String isbn) {
         BookResponse book = bookService.getBookDetail(isbn);
@@ -43,6 +51,7 @@ public class BookController {
     }
 
     // ISBN으로 해당 책의 작성 된 한줄평을 모두 조회
+    @Operation(summary = "책의 한줄평 조회", description = "ISBN을 기준으로 해당 책의 모든 한줄평을 페이지네이션 처리하여 조회합니다.")
     @GetMapping("/{isbn}/brief-reviews")
     public ResponseEntity<Page<String>> getAllBriefReviews(@PathVariable("isbn") String isbn,
         Pageable pageable) {
@@ -51,6 +60,7 @@ public class BookController {
     }
 
     // 좋아요가 가장 많은 책의 한줄평과 서평을 포함한 상세 정보 조회
+    @Operation(summary = "책의 상세 정보 및 서평 조회", description = "ISBN을 기준으로 좋아요가 가장 많은 서평을 포함한 책의 상세 정보를 조회합니다.")
     @GetMapping("/detail/{isbn}")
     public ResponseEntity<BookDetailResponse> getBookDetailReview(
         @PathVariable(name = "isbn") String isbn) {
