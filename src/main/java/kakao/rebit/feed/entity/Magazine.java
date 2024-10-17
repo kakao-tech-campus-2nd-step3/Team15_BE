2c820519128e0dc60e0fc14a31ffaf2534de27b8
Kangboom <jkl6147@naver.com>
2024-10-17 16:34:46 +0900

    feat: 이미지 조회 및 수정을 위한 ImageKeyModifier 인터페이스 구현

--- src/main/java/kakao/rebit/feed/entity/Magazine.java
+++ src/main/java/kakao/rebit/feed/entity/Magazine.java
@@ -4,16 +4,18 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import kakao.rebit.book.entity.Book;
-import kakao.rebit.common.domain.ImageKeyHolder;
+import kakao.rebit.common.domain.ImageKeyModifier;
 import kakao.rebit.member.entity.Member;
 
 @Entity
 @Table(name = "magazine")
 @DiscriminatorValue("M")
-public class Magazine extends Feed implements ImageKeyHolder {
+public class Magazine extends Feed implements ImageKeyModifier {
 
     private String name;
+
     private String imageKey;
+
     private String content;
 
     protected Magazine() {
