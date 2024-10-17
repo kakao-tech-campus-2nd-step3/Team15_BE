1cfdb64bce8279dd56721b940609ec2a6a0b941d
Kangboom <jkl6147@naver.com>
2024-10-17 16:52:05 +0900

    feat: MemberProfileResponse 변환 메서드 추가

--- src/main/java/kakao/rebit/member/mapper/MemberMapper.java
+++ src/main/java/kakao/rebit/member/mapper/MemberMapper.java
@@ -1,19 +1,36 @@
 package kakao.rebit.member.mapper;
 
+import kakao.rebit.member.dto.MemberProfileResponse;
 import kakao.rebit.member.dto.MemberResponse;
 import kakao.rebit.member.entity.Member;
+import kakao.rebit.s3.service.S3Service;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MemberMapper {
 
     public static MemberResponse toDto(Member member) {
         return new MemberResponse(
             member.getId(),
             member.getNickname(),
-            member.getImageUrl(),
+            member.getImageKey(),
             member.getBio(),
             member.getEmail(),
             member.getRole(),
             member.getPoints()
+        );
+    }
+
+    public MemberProfileResponse toMemberProfileResponse(Member member){
+        return new MemberProfileResponse(
+                member.getId(),
+                member.getNickname(),
+                member.getImageKey(),
+                s3Service.getDownloadUrl(member.getImageKey()).presignedUrl(),
+                member.getBio(),
+                member.getEmail(),
+                member.getRole(),
+                member.getPoints()
         );
     }
 }
