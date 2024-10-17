c19944b56c33ee6b61271e75894d24270038103e
Kangboom <jkl6147@naver.com>
2024-10-17 16:41:53 +0900

    feat: profile_image 필드 추가

--- src/main/java/kakao/rebit/auth/dto/KakaoUserInfo.java
+++ src/main/java/kakao/rebit/auth/dto/KakaoUserInfo.java
@@ -7,5 +7,8 @@ import com.fasterxml.jackson.databind.annotation.JsonNaming;
 public record KakaoUserInfo(Long id, KakaoAccount kakaoAccount, Properties properties) {
     public record KakaoAccount(String email) {}
 
-    public record Properties(String nickname) {}
+    public record Properties(
+            String nickname,
+            String profile_image
+    ) {}
 }
