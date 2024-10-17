d1e5f2ca291f3f29f2391c1cf658afa017b87338
Kangboom <jkl6147@naver.com>
2024-10-17 16:29:50 +0900

    feat: S3에 업로드 할 파일의 정보를 담은 dto 구현

---
+++ src/main/java/kakao/rebit/s3/dto/S3UploadFileInfo.java
@@ -0,0 +1,27 @@
+package kakao.rebit.s3.dto;
+
+import java.util.Arrays;
+import java.util.List;
+import kakao.rebit.s3.exception.S3NotAllowedFileFormatException;
+import kakao.rebit.utils.file.FileUtil;
+
+public record S3UploadFileInfo(
+        String filename,
+        String extension
+) {
+
+    private static final List<String> ALLOWED_FILE_FORMAT = Arrays.asList("jpg", "jpeg", "png", "gif", "svg", "webp");
+
+    public void validUploadImageFileFormat() {
+        if (!ALLOWED_FILE_FORMAT.contains(this.extension)) {
+            throw S3NotAllowedFileFormatException.EXCEPTION;
+        }
+    }
+
+    public static S3UploadFileInfo from(String fullFileName) {
+        return new S3UploadFileInfo(
+                FileUtil.extractFilename(fullFileName),
+                FileUtil.extractExtension(fullFileName)
+        );
+    }
+}
