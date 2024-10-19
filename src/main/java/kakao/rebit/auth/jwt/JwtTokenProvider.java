997dfdc80adf8258f61b53549574749661726775
Kangboom <jkl6147@naver.com>
2024-10-19 17:29:54 +0900

    refactor: jwt 커스텀 예외 사용

--- src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
+++ src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
@@ -8,6 +8,9 @@ import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.SignatureException;
 import java.util.Date;
 import javax.crypto.SecretKey;
+import kakao.rebit.auth.jwt.exception.ExpiredTokenException;
+import kakao.rebit.auth.jwt.exception.InvalidTokenException;
+import kakao.rebit.auth.jwt.exception.SignatureValidationFailedException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -42,23 +45,17 @@ public class JwtTokenProvider {
         return extractClaim(token).get("email", String.class);
     }
 
-    // 토큰에서 역할 추출
-    public String getRoleFromToken(String token) {
-        return extractClaim(token).get("role", String.class);
-    }
-
     public boolean validateToken(String token) {
         try {
             Jwts.parser().setSigningKey(key).build().parseClaimsJws(token);
             return true;
         } catch (SignatureException e) {
-            System.out.println("유효하지 않은 JWT 서명입니다.");
+            throw SignatureValidationFailedException.EXCEPTION;
         } catch (ExpiredJwtException e) {
-            System.out.println("만료된 JWT 토큰입니다.");
+            throw ExpiredTokenException.EXCEPTION;
         } catch (Exception e) {
-            System.out.println("유효하지 않은 JWT 토큰입니다.");
+            throw InvalidTokenException.EXCEPTION;
         }
-        return false;
     }
 
     // 토큰에서 클레임 정보 추출
