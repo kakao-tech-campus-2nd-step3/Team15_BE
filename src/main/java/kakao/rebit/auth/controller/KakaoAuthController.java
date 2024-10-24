8972198867b3575e7b0e71e89e7621294e02ec55
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-24 21:54:50 +0900

    refactor : 사용하지 않는 import 제거

--- src/main/java/kakao/rebit/auth/controller/KakaoAuthController.java
+++ src/main/java/kakao/rebit/auth/controller/KakaoAuthController.java
@@ -6,7 +6,6 @@ import kakao.rebit.auth.dto.LoginResponse;
 import kakao.rebit.auth.service.KakaoAuthService;
 import kakao.rebit.common.annotation.AllowAnonymous;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
