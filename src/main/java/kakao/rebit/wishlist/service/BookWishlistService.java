b86106ad1ca5ea6daacda05dc26d55b2715de596
seolbin96 <jsb3038@jnu.ac.kr>
2024-10-28 21:50:57 +0900

    feat : 메서드 수정 및 책이 없을 때 비즈니스 예외를 던지도록 수정

--- src/main/java/kakao/rebit/wishlist/service/BookWishlistService.java
+++ src/main/java/kakao/rebit/wishlist/service/BookWishlistService.java
@@ -1,6 +1,7 @@
 package kakao.rebit.wishlist.service;
 
 import kakao.rebit.book.entity.Book;
+import kakao.rebit.wishlist.exception.BookWishlistNotFoundException;
 import kakao.rebit.book.service.BookService;
 import kakao.rebit.member.entity.Member;
 import kakao.rebit.member.service.MemberService;
@@ -41,11 +42,12 @@ public class BookWishlistService {
 
     @Transactional
     public void deleteBookWishlist(Long memberId, String isbn) {
+        Member member = memberService.findMemberByIdOrThrow(memberId);
+        Book book = bookService.findByIsbnOrThrow(isbn);
 
-        memberService.findMemberByIdOrThrow(memberId);
-        bookService.findByIsbnOrThrow(isbn);
+        BookWishlist bookWishlist = bookWishlistRepository.findByMemberAndBook(member, book)
+            .orElseThrow(() -> BookWishlistNotFoundException.EXCEPTION);
 
-        bookWishlistRepository.findByMemberIdAndBookIsbn(memberId, isbn)
-            .ifPresent(bookWishlistRepository::delete);
+        bookWishlistRepository.delete(bookWishlist);
     }
 }
