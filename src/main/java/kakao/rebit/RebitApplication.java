59ab3b518579a33aaf7ed2ffe24975f78e1fec79
Profile-exe <kang67572346@gmail.com>
2024-11-01 02:01:50 +0900

    fix: JpaAuditing을 Application과 분리해 테스트 오류 방지

--- src/main/java/kakao/rebit/RebitApplication.java
+++ src/main/java/kakao/rebit/RebitApplication.java
@@ -2,9 +2,7 @@ package kakao.rebit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class RebitApplication {
 
