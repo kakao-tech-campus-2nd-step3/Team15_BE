e90e5c24c0858c2a73a5ff861002cb865d3e2bb4
Profile-exe <kang67572346@gmail.com>
2024-11-01 02:03:25 +0900

    feat: 회원 도메인 테스트를 위한 MemberFixture 구현

---
+++ src/testFixtures/java/kakao/rebit/member/fixture/values/MemberDefaultValues.java
@@ -0,0 +1,26 @@
+package kakao.rebit.member.fixture.values;
+
+import kakao.rebit.member.entity.Role;
+
+public record MemberDefaultValues(
+        Long id,
+        String nickname,
+        String imageKey,
+        String bio,
+        String email,
+        Role role,
+        Integer points,
+        String kakaoToken
+) {
+
+    public static final MemberDefaultValues INSTANCE = new MemberDefaultValues(
+            1L,
+            "테스트용 닉네임",
+            "default-image-key",
+            "테스트용 자기소개",
+            "test@email.com",
+            Role.ROLE_USER,
+            1000,
+            "kakao-token"
+    );
+}
