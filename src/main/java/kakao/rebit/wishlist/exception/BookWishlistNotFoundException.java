cf3a4d53b53d212d9ad2349349818154e9300f97
seolbin96 <jsb3038@jnu.ac.kr>
2024-11-01 14:37:25 +0900

    refactor : 불필요한 개행 제거

--- src/main/java/kakao/rebit/wishlist/exception/BookWishlistNotFoundException.java
+++ src/main/java/kakao/rebit/wishlist/exception/BookWishlistNotFoundException.java
@@ -3,8 +3,8 @@ package kakao.rebit.wishlist.exception;
 import kakao.rebit.common.exception.BusinessException;
 
 public class BookWishlistNotFoundException extends BusinessException {
-    public static final BookWishlistNotFoundException EXCEPTION =
-        new BookWishlistNotFoundException();
+
+    public static final BookWishlistNotFoundException EXCEPTION = new BookWishlistNotFoundException();
 
     private BookWishlistNotFoundException() {
         super(WishlistErrorCode.WISHLIST_NOT_FOUND);
