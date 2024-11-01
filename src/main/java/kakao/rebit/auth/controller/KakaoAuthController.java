dfc00be7050ebad5ba1128c94a4e40f29804f792
Kangboom <jkl6147@naver.com>
2024-11-01 14:18:08 +0900

    style: 불필요한 import 삭제

--- src/main/java/kakao/rebit/auth/controller/KakaoAuthController.java
+++ src/main/java/kakao/rebit/auth/controller/KakaoAuthController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
