d879e2fa6b22a7f920f348554fe21d0daf0a3f2e
Kangboom <jkl6147@naver.com>
2024-11-01 14:15:09 +0900

    feat: 비동기처리를 위한 @EnableAsync 추가

--- src/main/java/kakao/rebit/RebitApplication.java
+++ src/main/java/kakao/rebit/RebitApplication.java
@@ -3,7 +3,9 @@ package kakao.rebit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableJpaAuditing
 @SpringBootApplication
 public class RebitApplication {
