211f6b0a8e2fd97796e19989629e5a029b562937
Kangboom <jkl6147@naver.com>
2024-11-05 01:19:03 +0900

    refactor: 필요 없는 로직 삭제

--- src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
+++ src/main/java/kakao/rebit/auth/jwt/JwtTokenProvider.java
@@ -85,13 +85,6 @@ public class JwtTokenProvider {
         return token.substring(BEARER_PREFIX.length());
     }
 
-    public String extractTokenIfTokenPresent(String token){
-        if (token == null){
-            return null;
-        }
-        return extractToken(token);
-    }
-
     // 토큰에서 클레임 정보 추출
     private Claims extractClaim(String token) {
         return Jwts.parser()
