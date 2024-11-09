eac2e7736bb78388cb81361f3dbdc23c4e75404e
Kangboom <jkl6147@naver.com>
2024-11-10 01:12:54 +0900

    refactor: 검증 메서지 수정

--- src/main/java/kakao/rebit/feed/dto/request/create/CreateMagazineRequest.java
+++ src/main/java/kakao/rebit/feed/dto/request/create/CreateMagazineRequest.java
@@ -10,7 +10,7 @@ public class CreateMagazineRequest extends CreateFeedRequest {
     private String name;
 
     @NotBlank(message = "이미지는 필수입니다.")
-    @Pattern(regexp = "^feed" + ImageKeyModifier.BASE_IMAGE_KEY_FORMAT, message = "피드 imageKey는 'feed/%s/%s' 형식이어야 합니다.")
+    @Pattern(regexp = "^feed" + ImageKeyModifier.BASE_IMAGE_KEY_FORMAT, message = "피드 imageKey는 'feed/UUID/filename' 형식이어야 합니다.")
     private String imageKey;
 
     @NotBlank(message = "본문은 필수 입력 값입니다.")
--- src/main/java/kakao/rebit/feed/dto/request/update/UpdateStoryRequest.java
+++ src/main/java/kakao/rebit/feed/dto/request/update/UpdateStoryRequest.java
@@ -8,7 +8,7 @@ public record UpdateStoryRequest(
         Long bookId,
 
         @NotBlank(message = "이미지는 필수입니다.")
-        @Pattern(regexp = "^feed" + ImageKeyModifier.BASE_IMAGE_KEY_FORMAT, message = "피드 imageKey는 'feed/%s/%s' 형식이어야 합니다.")
+        @Pattern(regexp = "^feed" + ImageKeyModifier.BASE_IMAGE_KEY_FORMAT, message = "피드 imageKey는 'feed/UUID/filename' 형식이어야 합니다.")
         String imageKey,
 
         @NotBlank(message = "본문은 필수 입력 값입니다.")
