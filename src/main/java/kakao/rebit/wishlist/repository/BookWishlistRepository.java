982fa8d52be8a0b811ff2564b73b1d72c5edac62
seolbin96 <jsb3038@jnu.ac.kr>
2024-11-11 23:41:42 +0900

    refactor : 개행 추가

--- src/main/java/kakao/rebit/wishlist/repository/BookWishlistRepository.java
+++ src/main/java/kakao/rebit/wishlist/repository/BookWishlistRepository.java
@@ -12,8 +12,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface BookWishlistRepository extends JpaRepository<BookWishlist, Long> {
 
     Page<BookWishlist> findByMemberId(Long memberId, Pageable pageable);
+
     Optional<BookWishlist> findByMemberAndBook(Member member, Book book);
+
     boolean existsByMemberAndBook(Member member, Book book);
+
     default BookWishlist findByMemberAndBookOrThrow(Member member, Book book) {
         return findByMemberAndBook(member, book)
                 .orElseThrow(() -> BookWishlistNotFoundException.EXCEPTION);
