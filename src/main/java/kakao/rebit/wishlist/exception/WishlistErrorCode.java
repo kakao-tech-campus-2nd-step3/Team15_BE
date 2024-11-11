580f5cd398eb2addc45b698a144bccb072ded5ba
seolbin96 <jsb3038@jnu.ac.kr>
2024-11-11 23:26:23 +0900

    feat : 위시리스트에 책이 없는 경우 에러코드 수정 및 에러코드 추가

--- src/main/java/kakao/rebit/wishlist/exception/WishlistErrorCode.java
+++ src/main/java/kakao/rebit/wishlist/exception/WishlistErrorCode.java
@@ -4,7 +4,9 @@ import kakao.rebit.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum WishlistErrorCode implements ErrorCode {
-    WISHLIST_NOT_FOUND("WL001", HttpStatus.NOT_FOUND, "위시리스트에서 해당 책을 찾을 수 없습니다.");
+    WISHLIST_BOOK_NOT_FOUND("WL001", HttpStatus.NOT_FOUND, "위시리스트에서 해당 책을 찾을 수 없습니다."),
+    WISHLIST_CHALLENGE_NOT_FOUND("WL002", HttpStatus.NOT_FOUND, "위시리스트에서 해당 챌린지를 찾을 수 없습니다."),
+    ALREADY_IN_WISHLIST("WL003", HttpStatus.CONFLICT, "이미 위시리스트에 존재합니다.");
 
     private final String code;
     private final HttpStatus httpStatus;
