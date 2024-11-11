14766298181720c79545d55fcdfe8fa2b13012e3
seolbin96 <jsb3038@jnu.ac.kr>
2024-11-11 23:49:02 +0900

    refactor : 이미 위시리스트에 존재할 경우 예외 던지도록 수정 및 해당 항목이 위시리스트에 존재하는지 확인하는 isWishListed 추가

--- src/main/java/kakao/rebit/wishlist/service/BookWishlistService.java
+++ src/main/java/kakao/rebit/wishlist/service/BookWishlistService.java
@@ -4,7 +4,9 @@ import kakao.rebit.book.entity.Book;
 import kakao.rebit.book.service.BookService;
 import kakao.rebit.member.entity.Member;
 import kakao.rebit.member.service.MemberService;
+import kakao.rebit.wishlist.dto.response.BookWishlistResponse;
 import kakao.rebit.wishlist.entity.BookWishlist;
+import kakao.rebit.wishlist.exception.AlreadyInWishlistException;
 import kakao.rebit.wishlist.repository.BookWishlistRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,23 +21,32 @@ public class BookWishlistService {
     private final BookService bookService;
 
     public BookWishlistService(BookWishlistRepository bookWishlistRepository,
-        MemberService memberService,
-        BookService bookService) {
+            MemberService memberService,
+            BookService bookService) {
         this.bookWishlistRepository = bookWishlistRepository;
         this.memberService = memberService;
         this.bookService = bookService;
     }
 
     @Transactional(readOnly = true)
-    public Page<String> getBookWishlist(Long memberId, Pageable pageable) {
+    public Page<BookWishlistResponse> getBookWishlist(Long memberId, Pageable pageable) {
         return bookWishlistRepository.findByMemberId(memberId, pageable)
-            .map(bookWishlist -> bookWishlist.getBook().getIsbn());
+                .map(bookWishlist -> new BookWishlistResponse(
+                        bookWishlist.getBook().getIsbn(),
+                        true
+                ));
     }
 
     @Transactional
     public void addBookWishlist(Long memberId, String isbn) {
         Member member = memberService.findMemberByIdOrThrow(memberId);
         Book book = bookService.findByIsbnOrThrow(isbn);
+
+        // 이미 위시리스트에 존재하는 경우 예외 던지기
+        if (bookWishlistRepository.existsByMemberAndBook(member, book)) {
+            throw AlreadyInWishlistException.EXCEPTION;
+        }
+
         bookWishlistRepository.save(new BookWishlist(member, book));
     }
 
--- src/main/java/kakao/rebit/wishlist/service/ChallengeWishlistService.java
+++ src/main/java/kakao/rebit/wishlist/service/ChallengeWishlistService.java
@@ -4,7 +4,11 @@ import kakao.rebit.challenge.entity.Challenge;
 import kakao.rebit.challenge.repository.ChallengeRepository;
 import kakao.rebit.member.entity.Member;
 import kakao.rebit.member.repository.MemberRepository;
+import kakao.rebit.member.service.MemberService;
+import kakao.rebit.challenge.service.ChallengeService;
+import kakao.rebit.wishlist.dto.response.ChallengeWishlistResponse;
 import kakao.rebit.wishlist.entity.ChallengeWishlist;
+import kakao.rebit.wishlist.exception.AlreadyInWishlistException;
 import kakao.rebit.wishlist.repository.ChallengeWishlistRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,34 +21,53 @@ public class ChallengeWishlistService {
     private final ChallengeWishlistRepository challengeWishlistRepository;
     private final MemberRepository memberRepository;
     private final ChallengeRepository challengeRepository;
+    private final MemberService memberService;
+    private final ChallengeService challengeService;
 
-    public ChallengeWishlistService(ChallengeWishlistRepository challengeWishlistRepository, MemberRepository memberRepository, ChallengeRepository challengeRepository) {
+    public ChallengeWishlistService(ChallengeWishlistRepository challengeWishlistRepository,
+            MemberRepository memberRepository, ChallengeRepository challengeRepository, MemberService memberService,
+            ChallengeService challengeService) {
         this.challengeWishlistRepository = challengeWishlistRepository;
         this.memberRepository = memberRepository;
         this.challengeRepository = challengeRepository;
+        this.memberService = memberService;
+        this.challengeService = challengeService;
     }
 
     @Transactional(readOnly = true)
-    public Page<Long> getChallengeWishlist(Long memberId, Pageable pageable) {
-        return challengeWishlistRepository.findByMemberId(memberId, pageable)
-            .map(challengeWishlist -> challengeWishlist.getChallenge().getId());
+    public Page<ChallengeWishlistResponse> getChallengeWishlist(Long memberId, Pageable pageable) {
+        Page<ChallengeWishlist> challengeWishlists = challengeWishlistRepository.findByMemberId(memberId, pageable);
+
+        return challengeWishlists.map(challengeWishlist -> {
+            boolean isWishlisted = true;
+            return new ChallengeWishlistResponse(
+                    challengeWishlist.getChallenge().getId(),
+                    isWishlisted
+            );
+        });
     }
 
     @Transactional
     public void addChallengeWishlist(Long memberId, Long challengeId) {
         Member member = memberRepository.findById(memberId)
-            .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
         Challenge challenge = challengeRepository.findById(challengeId)
-            .orElseThrow(() -> new IllegalArgumentException("Challenge not found"));
+                .orElseThrow(() -> new IllegalArgumentException("Challenge not found"));
+
+        // 이미 위시리스트에 존재하는 경우 예외 던지기
+        if (challengeWishlistRepository.existsByMemberAndChallenge(member, challenge)) {
+            throw AlreadyInWishlistException.EXCEPTION;
+        }
+
         challengeWishlistRepository.save(new ChallengeWishlist(member, challenge));
     }
 
     @Transactional
     public void deleteChallengeWishlist(Long memberId, Long challengeId) {
-        challengeWishlistRepository.findAll().stream()
-            .filter(challengeWishlist -> challengeWishlist.getMember().getId().equals(memberId)
-                && challengeWishlist.getChallenge().getId().equals(challengeId))
-            .findFirst()
-            .ifPresent(challengeWishlistRepository::delete);
+        ChallengeWishlist challengeWishlist = challengeWishlistRepository
+                .findByMemberAndChallengeOrThrow(memberService.findMemberByIdOrThrow(memberId),
+                        challengeService.findChallengeByIdOrThrow(challengeId));
+
+        challengeWishlistRepository.delete(challengeWishlist);
     }
 }
