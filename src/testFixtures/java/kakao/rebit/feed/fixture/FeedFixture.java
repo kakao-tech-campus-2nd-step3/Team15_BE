d5a6327c6b73e9405935769882b608b145ed87cd
Kangboom <jkl6147@naver.com>
2024-11-13 12:13:15 +0900

    test: 피드 TextFixture 추가 및 수정

--- src/testFixtures/java/kakao/rebit/feed/fixture/FeedFixture.java
+++ src/testFixtures/java/kakao/rebit/feed/fixture/FeedFixture.java
@@ -1,6 +1,12 @@
 package kakao.rebit.feed.fixture;
 
 import kakao.rebit.book.entity.Book;
+import kakao.rebit.feed.dto.request.create.CreateFavoriteBookRequest;
+import kakao.rebit.feed.dto.request.create.CreateMagazineRequest;
+import kakao.rebit.feed.dto.request.create.CreateStoryRequest;
+import kakao.rebit.feed.dto.request.update.UpdateFavoriteBookRequest;
+import kakao.rebit.feed.dto.request.update.UpdateMagazineRequest;
+import kakao.rebit.feed.dto.request.update.UpdateStoryRequest;
 import kakao.rebit.feed.entity.FavoriteBook;
 import kakao.rebit.feed.entity.Magazine;
 import kakao.rebit.feed.entity.Story;
@@ -10,6 +16,59 @@ import kakao.rebit.feed.fixture.values.StoryDefaultValues;
 import kakao.rebit.member.entity.Member;
 
 public class FeedFixture {
+
+    public static CreateFavoriteBookRequest createFavoriteBookRequestWithBookId(Long bookId) {
+        return new CreateFavoriteBookRequest(
+                FavoriteBookDefaultValues.INSTANCE.type(),
+                bookId,
+                "update-briefReview",
+                "update-fullReview"
+        );
+    }
+
+    public static CreateMagazineRequest createMagazineRequestWithBookId(Long bookId) {
+        return new CreateMagazineRequest(
+                MagazineDefaultValues.INSTANCE.type(),
+                bookId,
+                MagazineDefaultValues.INSTANCE.name(),
+                MagazineDefaultValues.INSTANCE.imageKey(),
+                "update-content"
+        );
+    }
+
+    public static CreateStoryRequest createStoryRequestWithBookId(Long bookId) {
+        return new CreateStoryRequest(
+                StoryDefaultValues.INSTANCE.type(),
+                bookId,
+                StoryDefaultValues.INSTANCE.imageKey(),
+                StoryDefaultValues.INSTANCE.content()
+        );
+    }
+
+    public static UpdateFavoriteBookRequest updateFavoriteBookRequestWithBookId(Long bookId) {
+        return new UpdateFavoriteBookRequest(
+                bookId,
+                FavoriteBookDefaultValues.INSTANCE.briefReview(),
+                FavoriteBookDefaultValues.INSTANCE.fullReview()
+        );
+    }
+
+    public static UpdateMagazineRequest updateMagazineRequestWithBookId(Long bookId) {
+        return new UpdateMagazineRequest(
+                bookId,
+                MagazineDefaultValues.INSTANCE.imageKey(),
+                MagazineDefaultValues.INSTANCE.name(),
+                MagazineDefaultValues.INSTANCE.content()
+        );
+    }
+
+    public static UpdateStoryRequest updateStoryRequestWithBookId(Long bookId) {
+        return new UpdateStoryRequest(
+                bookId,
+                StoryDefaultValues.INSTANCE.imageKey(),
+                "update-content"
+        );
+    }
 
     public static FavoriteBook createFavoriteBook(Member member, Book book) {
         FavoriteBookDefaultValues defaults = FavoriteBookDefaultValues.INSTANCE;
