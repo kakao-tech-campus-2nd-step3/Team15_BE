820197e3372473c2c92e595729466208eded6fa3
Kangboom <jkl6147@naver.com>
2024-11-13 18:23:34 +0900

    feat: date 필드 추가

--- src/main/java/kakao/rebit/diary/dto/DiaryResponse.java
+++ src/main/java/kakao/rebit/diary/dto/DiaryResponse.java
@@ -1,5 +1,11 @@
 package kakao.rebit.diary.dto;
 
-import java.time.LocalDateTime;
+public record DiaryResponse(
+        Long id,
+        String content,
+        Long memberId,
+        String isbn,
+        String date
+) {
 
-public record DiaryResponse(Long id, String content, Long memberId, String isbn, LocalDateTime createAt) {}
+}
