d626037ecb3172822f3f7524930b0f7ba8b52f9f
Kangboom <jkl6147@naver.com>
2024-11-13 12:11:25 +0900

    feat: 역직렬화를 위한 @JsonProperty 추가

--- src/main/java/kakao/rebit/feed/dto/response/FavoriteBookResponse.java
+++ src/main/java/kakao/rebit/feed/dto/response/FavoriteBookResponse.java
@@ -1,5 +1,6 @@
 package kakao.rebit.feed.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "인생책 응답")
@@ -8,12 +9,15 @@ public class FavoriteBookResponse extends FeedResponse {
     private final String briefReview;
     private final String fullReview;
 
-    public FavoriteBookResponse(Long id, FeedAuthorResponse author, FeedBookResponse book,
-            String type,
-            int likes,
-            boolean isLiked,
-            String briefReview,
-            String fullReview) {
+    public FavoriteBookResponse(
+            @JsonProperty("id") Long id,
+            @JsonProperty("author") FeedAuthorResponse author,
+            @JsonProperty("book") FeedBookResponse book,
+            @JsonProperty("type") String type,
+            @JsonProperty("likes") int likes,
+            @JsonProperty("isLiked") boolean isLiked,
+            @JsonProperty("briefReview") String briefReview,
+            @JsonProperty("fullReview") String fullReview) {
         super(id, author, book, type, likes, isLiked);
         this.briefReview = briefReview;
         this.fullReview = fullReview;
--- src/test/java/kakao/rebit/common/PageTemplate.java
+++ src/test/java/kakao/rebit/common/PageTemplate.java
@@ -1,6 +1,7 @@
 package kakao.rebit.common;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -8,7 +9,12 @@ import org.springframework.data.domain.PageRequest;
 @JsonIgnoreProperties(ignoreUnknown = true, value = {"pageable"})
 public class PageTemplate<T> extends PageImpl<T> {
 
-    public PageTemplate(List<T> content, int number, int size, long totalElements) {
+    public PageTemplate(
+            @JsonProperty("content") List<T> content,
+            @JsonProperty("number") int number,
+            @JsonProperty("size") int size,
+            @JsonProperty("totalElements") long totalElements
+    ) {
         super(content, PageRequest.of(number, size), totalElements);
     }
 }
