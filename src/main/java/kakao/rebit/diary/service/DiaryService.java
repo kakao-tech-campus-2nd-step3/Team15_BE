928ee45d9ec2f7cc47d6ba0550622bc34720eeae
Kangboom <jkl6147@naver.com>
2024-11-13 17:47:28 +0900

    feat: createAt 필드 추가

--- src/main/java/kakao/rebit/diary/service/DiaryService.java
+++ src/main/java/kakao/rebit/diary/service/DiaryService.java
@@ -23,7 +23,7 @@ public class DiaryService {
     private final BookRepository bookRepository;
 
     public DiaryService(DiaryRepository diaryRepository, MemberService memberService,
-        BookRepository bookRepository) {
+            BookRepository bookRepository) {
         this.diaryRepository = diaryRepository;
         this.memberService = memberService;
         this.bookRepository = bookRepository;
@@ -32,17 +32,19 @@ public class DiaryService {
     @Transactional(readOnly = true)
     public Page<DiaryResponse> getDiaries(Long memberId, Pageable pageable) {
         return diaryRepository.findByMemberId(memberId, pageable)
-            .map(diary -> new DiaryResponse(diary.getId(), diary.getContent(),
-                diary.getMember().getId(),
-                diary.getBook().getIsbn()));
+                .map(diary -> new DiaryResponse(diary.getId(), diary.getContent(),
+                        diary.getMember().getId(),
+                        diary.getBook().getIsbn(),
+                        diary.getCreatedAt()
+                ));
     }
 
     @Transactional(readOnly = true)
     public DiaryResponse getDiaryById(Long memberId, Long id) {
         Diary diary = diaryRepository.findByIdAndMemberId(id, memberId)
-            .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
+                .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
         return new DiaryResponse(diary.getId(), diary.getContent(), diary.getMember().getId(),
-            diary.getBook().getIsbn());
+                diary.getBook().getIsbn(), diary.getCreatedAt());
     }
 
     @Transactional
@@ -50,7 +52,7 @@ public class DiaryService {
         Member member = memberService.findMemberByIdOrThrow(memberId);
 
         Book book = bookRepository.findByIsbn(diaryRequest.isbn())
-            .orElseThrow(() -> BookNotFoundException.EXCEPTION);
+                .orElseThrow(() -> BookNotFoundException.EXCEPTION);
 
         Diary diary = new Diary(diaryRequest.content(), member, book);
         Diary savedDiary = diaryRepository.save(diary);
@@ -60,10 +62,10 @@ public class DiaryService {
     @Transactional
     public void updateDiary(Long memberId, Long id, DiaryRequest diaryRequest) {
         Diary diary = diaryRepository.findByIdAndMemberId(id, memberId)
-            .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
+                .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
 
         Book book = bookRepository.findByIsbn(diaryRequest.isbn())
-            .orElseThrow(() -> BookNotFoundException.EXCEPTION);
+                .orElseThrow(() -> BookNotFoundException.EXCEPTION);
 
         diary.updateDiary(diaryRequest.content(), book);
     }
@@ -71,7 +73,7 @@ public class DiaryService {
     @Transactional
     public void deleteDiary(Long memberId, Long id) {
         Diary diary = diaryRepository.findByIdAndMemberId(id, memberId)
-            .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
+                .orElseThrow(() -> DiaryNotFoundException.EXCEPTION);
         diaryRepository.delete(diary);
     }
 }
