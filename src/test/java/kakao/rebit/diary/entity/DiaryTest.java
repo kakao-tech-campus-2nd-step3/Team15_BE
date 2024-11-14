62a7dfea836da5a297af00ba338a4c53256b2845
seolbin96 <jsb3038@jnu.ac.kr>
2024-11-14 19:43:23 +0900

    refactor : 주석 추가

--- src/test/java/kakao/rebit/diary/entity/DiaryTest.java
+++ src/test/java/kakao/rebit/diary/entity/DiaryTest.java
@@ -25,13 +25,14 @@ class DiaryTest {
 
     @Test
     void 다이어리_내용_수정_성공() {
-        // 새로운 book 생성 후 다이어리 내용과 책을 업데이트
+        // given
         Book newBook = BookFixture.createBookWithIsbn("9876543210");
         String newContent = "수정된 내용";
 
+        // when
         diary.updateDiary(newContent, newBook);
 
-        // 업데이트 후 변경 사항이 적용되었는지 확인
+        // then
         assertEquals(newContent, diary.getContent());
         assertEquals(newBook, diary.getBook());
     }
